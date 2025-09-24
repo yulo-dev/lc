4a124662f046baa046f777819fa35ada6d0be46e
Yulo L. <noelleloyu@gmail.com>
2025-09-24 00:55:26 -0700

    Time: 2 ms (38.94%), Space: 17.7 MB (95.63%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,10 +1,6 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
         res = []
-        if not nums:
-            return []
-            
         visited = [False] * len(nums)
         self.dfs(nums, visited, [], res)
         return res
@@ -12,15 +8,14 @@ class Solution:
     def dfs(self, nums, visited, subset, res):
         if len(subset) == len(nums):
             res.append(subset[:])
+            return
 
         for i in range(len(nums)):
-            if visited[i] == True:
-                continue
+            if visited[i] != True:
+                subset.append(nums[i])
+                visited[i] = True
 
-            subset.append(nums[i])
-            visited[i] = True
+                self.dfs(nums, visited, subset, res)
 
-            self.dfs(nums, visited, subset, res)
-
-            subset.pop()
-            visited[i] = False
+                subset.pop()
+                visited[i] = False
