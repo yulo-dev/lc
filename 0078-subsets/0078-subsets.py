6f8f1db70299b0dc464a1700630d1b26e245611a
Yulo L. <noelleloyu@gmail.com>
2025-09-24 14:50:29 -0700

    Time: 4 ms (4.73%), Space: 17.7 MB (98.99%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,9 +4,10 @@ class Solution:
         self.dfs(nums, 0, [], res)
         return res
 
-    def dfs(self, nums, k, subset, res):
+    def dfs(self, nums, start, subset, res):
         res.append(subset[:])
-        for i in range(k, len(nums)):
+        for i in range(start, len(nums)):
             subset.append(nums[i])
             self.dfs(nums, i+1, subset, res)
             subset.pop()
+            
