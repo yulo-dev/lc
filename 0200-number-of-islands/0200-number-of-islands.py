01b1c20cb20307419eda8657265a94f1da889140
Yulo L. <noelleloyu@gmail.com>
2025-09-23 21:13:08 -0700

    Time: 298 ms (13.49%), Space: 25.5 MB (17.87%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        islands = 0
+        visited = set()
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    self.bfs(grid, i, j, visited)
+                    islands += 1
+        return islands
+
+    def bfs(self, grid, x, y, visited):
+        queue = deque([(x, y)])
+        visited.add((x, y))
+        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while queue:
+            x, y = queue.popleft()
+            for dir_x, dir_y in DIRECTIONS:
+                new_x = x + dir_x
+                new_y = y + dir_y
+
+                if not self.is_valid(grid, new_x, new_y, visited):
+                    continue
+                queue.append((new_x, new_y))
+                visited.add((new_x, new_y))
+    
+    def is_valid(self, grid, x, y, visited):
+        m = len(grid)
+        n = len(grid[0])
+        if not (0 <= x < m) or not (0 <= y < n):
+            return False
+        if (x, y) in visited:
+            return False
+        return grid[x][y] == "1"
