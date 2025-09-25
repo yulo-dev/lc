2df6899f74ea8e9a8efaa0f2355a9a575c159fd8
Yulo L. <noelleloyu@gmail.com>
2025-09-24 20:42:26 -0700

    Time: 4971 ms (26.39%), Space: 18.1 MB (21.9%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,23 +2,25 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        def check(i, j, k):
-            if board[i][j] != word[k]:
+        def check(x, y, k):
+            if board[x][y] != word[k]:
                 return False
             if k == len(word) - 1:
                 return True
             
-            visited.add((i, j))
+            visited.add((x, y))
             result = False
-            for di, dj in directions:
-                newi, newj = i + di, j + dj
-                if 0 <= newi < len(board) and 0 <= newj < len(board[0]):
-                    if (newi, newj) not in visited:
-                        if check(newi, newj, k + 1):
+
+            for dir_x, dir_y in directions:
+                new_x = x + dir_x
+                new_y = y + dir_y
+                if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]):
+                    if (new_x, new_y) not in visited:
+                        if check(new_x, new_y, k + 1):
                             result = True
                             break
             
-            visited.remove((i, j))
+            visited.remove((x, y))
             return result
 
         m, n = len(board), len(board[0])
