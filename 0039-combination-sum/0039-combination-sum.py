1a0edad32d021ed901b9a75aa9cbcb82c8d14754
Yulo L. <noelleloyu@gmail.com>
2025-09-24 17:50:02 -0700

    Time: 12 ms (37.16%), Space: 17.6 MB (99.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,17 +3,15 @@ class Solution:
         res = []
         if not candidates:
             return []
-
         self.dfs(candidates, target, 0, [], res)
         return res
 
     def dfs(self, candidates, target, start, subset, res):
         if target == 0:
             return res.append(subset[:])
-
         if target < 0:
             return
-
+        
         for i in range(start, len(candidates)):
             subset.append(candidates[i])
             self.dfs(candidates, target - candidates[i], i, subset, res)
