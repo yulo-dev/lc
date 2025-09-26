ab6bc69f2572475cc0eb1e89363a678665b50f32
Yulo L. <noelleloyu@gmail.com>
2025-09-25 21:08:11 -0700

    Time: 7 ms (24.42%), Space: 18 MB (5.53%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,49 +1,54 @@
-from collections import deque 
+from collections import deque
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0]:
             return 0
 
-        fresh = 0
+        fresh = 0 
         queue = deque([])
         visited = set()
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == 2:
+                val = grid[i][j]
+                if val == 2:
                     queue.append((i, j, 0))
                     visited.add((i, j))
-                elif grid[i][j] == 1:
+                elif val == 1:
                     fresh += 1
 
-        fresh, minutes = self.bfs(grid, queue, visited, fresh)
+        minutes, fresh = self.bfs(grid, queue, fresh, visited)
         return minutes if fresh == 0 else -1
 
-    
-    def bfs(self, grid, queue, visited, fresh):
-
-        DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
+    def bfs(self, grid, queue, fresh, visited):
+        DIRECTIONS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         minutes = 0
+
         while queue:
             x, y, minute = queue.popleft()
-            minutes = max(minutes, minute)
+            minutes = max(minute, minutes)
             for dir_x, dir_y in DIRECTIONS:
                 new_x = x + dir_x
                 new_y = y + dir_y
                 if not self.is_valid(grid, new_x, new_y, visited):
                     continue
+                fresh -= 1
                 queue.append((new_x, new_y, minute + 1))
                 visited.add((new_x, new_y))
-                fresh -= 1
 
-        return fresh, minutes
+        return minutes, fresh
 
     def is_valid(self, grid, x, y, visited):
         m = len(grid)
         n = len(grid[0])
+
         if not (0 <= x < m) or not (0 <= y < n):
             return False
         if (x, y) in visited:
             return False
         return grid[x][y] == 1
+        
+                
+                    
+
