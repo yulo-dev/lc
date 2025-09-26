fe590846d29739e4b441f6824da0add46de5a4b2
Yulo L. <noelleloyu@gmail.com>
2025-09-26 00:01:17 -0700

    Time: 5251 ms (21.19%), Space: 17.7 MB (87.69%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -10,15 +10,19 @@ class Solution:
         return False
 
     def check(self, board, x, y, k, word, visited):
+
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
         if board[x][y] != word[k]:
-            return False
+            return False # ← 字母不符合直接 return False, 因為這裡還沒加進 visited 所以不需要回溯
+
         if k == len(word) - 1:
-            return True
+            return True 
             
         visited.add((x, y))
         result = False
 
+        # 本身字符合 就往他四個方向檢查
         for dir_x, dir_y in directions:
             new_x = x + dir_x
             new_y = y + dir_y
@@ -28,5 +32,6 @@ class Solution:
                         result = True
                         break
             
+        # 如果本身符合但後續的字不符合 那就要慢慢backtrack 把這些原本被放進visited的字拿掉
         visited.remove((x, y))
         return result
