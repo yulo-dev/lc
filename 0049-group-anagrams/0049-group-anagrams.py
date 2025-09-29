7dca05d251c254694f3395709adb7b0b3c225403
Yulo L. <noelleloyu@gmail.com>
2025-09-28 21:11:58 -0700

    Time: 12 ms (60.52%), Space: 20.4 MB (97.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,9 +2,14 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if not strs:
+            return []
+        
         freq = defaultdict(list)
 
         for ch in strs:
             key = ''.join(sorted(ch))
             freq[key].append(ch)
+        
         return list(freq.values())
+        
