a298278c861beb4711603249f2327be4675b2a5f
Yulo L. <noelleloyu@gmail.com>
2025-10-01 21:19:50 -0700

    Time: 8 ms (52.79%), Space: 18 MB (82.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,20 +4,19 @@ class Solution:
             return True
 
         left = 0
-        right = len(s) -1
+        right = len(s) - 1
 
         while left < right:
             while left < right and not s[left].isalnum():
                 left += 1
+            
             while left < right and not s[right].isalnum():
                 right -= 1
 
             if s[left].lower() != s[right].lower():
                 return False
-            
+
             left += 1
             right -= 1
-
+        
         return True
-
-            
