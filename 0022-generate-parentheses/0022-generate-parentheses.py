fb22fa854fb0075729579efecb33c7ae523f88cc
Yulo L. <noelleloyu@gmail.com>
2025-10-04 16:06:50 -0700

    Time: 0 ms (100%), Space: 18.1 MB (25.44%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,27 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        return self.bfs(n)
-
-    def bfs(self, n):
         res = []
-        queue = deque()
-        # 每個元素存 (當前字串, open數量, close數量)
-        queue.append(("", 0, 0))
+        queue = deque([("", 0, 0)])
 
         while queue:
-            cur, open_cnt, close_cnt = queue.popleft()
-
-            # 如果長度 == 2*n，代表完成一個有效字串
-            if len(cur) == 2 * n:
-                res.append(cur)
+            substring, open_cnt, close_cnt = queue.popleft()
+            if len(substring) == (n*2):
+                res.append(substring)
                 continue
-
-            # 可以加 "("
             if open_cnt < n:
-                queue.append((cur + "(", open_cnt + 1, close_cnt))
-
-            # 可以加 ")"
+                queue.append((substring + "(", open_cnt+1, close_cnt))
             if close_cnt < open_cnt:
-                queue.append((cur + ")", open_cnt, close_cnt + 1))
-
+                queue.append((substring + ")", open_cnt, close_cnt+1))
         return res
+
+        
