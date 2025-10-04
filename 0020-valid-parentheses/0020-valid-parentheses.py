24c3981988e3dce9c0075bf73c4e609db6b893f8
Yulo L. <noelleloyu@gmail.com>
2025-10-04 13:47:18 -0700

    Time: 0 ms (100%), Space: 17.7 MB (84.67%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,8 +4,9 @@ class Solution:
         stack = []
         for ch in s:
             if ch in mapping:
-                if not stack or mapping[ch] != stack.pop():
+                if not stack or mapping[ch] != stack[-1]:
                     return False
+                stack.pop()
             else:
                 stack.append(ch)
         return not stack
