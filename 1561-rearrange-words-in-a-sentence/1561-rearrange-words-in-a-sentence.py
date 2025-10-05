9b485e5e701b00320a933e630e9cf239f01233e0
Yulo L. <noelleloyu@gmail.com>
2025-10-05 15:26:19 -0700

    Time: 8 ms (59.42%), Space: 20.1 MB (38.64%) - LeetHub

--- 1561-rearrange-words-in-a-sentence/1561-rearrange-words-in-a-sentence.py
+++ 1561-rearrange-words-in-a-sentence/1561-rearrange-words-in-a-sentence.py
@@ -1,7 +1,7 @@
 class Solution:
     def arrangeWords(self, text: str) -> str:
         lower_text = text.lower()
-        word_spilt = lower_text.split(" ")
+        word_spilt = lower_text.split() #default is split by space
         word_sort = sorted(word_spilt, key = len)
         res = ' '.join(word_sort)
         return res.capitalize()
