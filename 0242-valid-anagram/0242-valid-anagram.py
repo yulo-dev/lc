69267d6a4a4aa6e1cb8c1677eac2eac23ced7d83
Yulo L. <noelleloyu@gmail.com>
2025-10-05 17:07:38 -0700

    Time: 8 ms (78.83%), Space: 17.9 MB (49.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,3 @@
-
-from collections import defaultdict
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        s_freq = defaultdict(int)
-        t_freq = defaultdict(int)
-
-        for s_ch in s:
-            s_freq[s_ch] += 1
-
-        for t_ch in t:
-            t_freq[t_ch] += 1
-
-        return s_freq == t_freq
+      return Counter(s) == Counter(t)
