1f8f4d582ad1e4f3a338159a8d07a06b154a15cc
Yulo L. <noelleloyu@gmail.com>
2025-10-07 12:08:59 -0700

    Time: 3 ms (30.31%), Space: 18 MB (43.6%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -6,12 +6,11 @@ class Solution:
 
         while queue:
             substring, open_cnt, close_cnt = queue.popleft()
-            if len(substring) == (2*n):
+            if len(substring) == (n*2):
                 res.append(substring)
                 continue
             if open_cnt < n:
-                queue.append((substring + "(", open_cnt + 1, close_cnt))
+                queue.append((substring+"(", open_cnt+1, close_cnt))
             if close_cnt < open_cnt:
-                queue.append((substring + ")", open_cnt, close_cnt + 1))
+                queue.append((substring+")", open_cnt, close_cnt+1))
         return res
-
