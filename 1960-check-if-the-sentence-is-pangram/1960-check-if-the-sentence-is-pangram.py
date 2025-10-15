8d7165285c49dbb8e07c38ab174686636010e7c9
Yulo L. <noelleloyu@gmail.com>
2025-10-15 00:24:49 -0700

    Time: 0 ms (100%), Space: 17.7 MB (51.05%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -1,5 +1,5 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         alpha = "abcdefghijklmnopqrstuvwxyz"
-        sentence_sort = ''.join(sorted(set(sentence)))
+        sentence_sort = ''.join(sorted(set(sentence.lower())))
         return alpha == sentence_sort
