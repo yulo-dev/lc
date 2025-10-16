92e2228caa4180f8092a1b56165fd6108d5822b3
Yulo L. <noelleloyu@gmail.com>
2025-10-15 23:09:03 -0700

    Time: 33 ms (50.07%), Space: 20.5 MB (38.91%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,13 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        cnt = 0
         prefix_sum = 0
-        visited = {0: 1}
+        cnt = 0
+        visited = {0:1}
 
         for num in nums:
             prefix_sum += num
             if prefix_sum - k in visited:
                 cnt += visited[prefix_sum - k]
+
             visited[prefix_sum] = visited.get(prefix_sum, 0) + 1
+
         return cnt
