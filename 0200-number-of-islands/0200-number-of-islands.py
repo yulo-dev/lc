9b5d8d510b8683d9cf3213b184de0bf5dd606a6e
Yulo L. <noelleloyu@gmail.com>
2025-10-15 20:45:55 -0700

    Time: 283 ms (19.57%), Space: 25.4 MB (21.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,34 +4,36 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid or not grid[0]:
             return 0
-
-        islands = 0
+        
+        island = 0
         visited = set()
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i,j) not in visited:
+                if grid[i][j] == "1" and (i, j) not in visited:
                     self.bfs(grid, i, j, visited)
-                    islands += 1
-        return islands
+                    island += 1
+        return island
 
     def bfs(self, grid, x, y, visited):
-        DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        DIRECTIONS = [(0,1), (0,-1), (1,0), (-1,0)]
         queue = deque([(x,y)])
         visited.add((x,y))
 
         while queue:
             x, y = queue.popleft()
             for dir_x, dir_y in DIRECTIONS:
-                new_x = x + dir_x
-                new_y = y + dir_y
+                new_x = dir_x + x
+                new_y = dir_y + y
 
                 if not self.is_valid(grid, new_x, new_y, visited):
                     continue
                 queue.append([new_x, new_y])
                 visited.add((new_x, new_y))
-    
+                
+
     def is_valid(self, grid, x, y, visited):
+
         m = len(grid)
         n = len(grid[0])
 
@@ -40,5 +42,5 @@ class Solution:
         
         if (x, y) in visited:
             return False
-        
+
         return grid[x][y] == "1"
