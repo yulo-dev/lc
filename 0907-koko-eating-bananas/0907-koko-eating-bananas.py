4e13e8b4b1d946535101d0c7fe92be3ff20234f1
Yulo L. <noelleloyu@gmail.com>
2025-10-22 22:49:15 -0700

    Time: 149 ms (80.37%), Space: 19.2 MB (35.46%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,23 +1,29 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        
-        left = 1           # Minimum possible eating speed
-        right = max(piles) # Maximum needed eating speed
+        #1. get the mid from 1~max piles 
+        #2. create a while loop: 
+            #2a. create for loop: count the hours needed based on mid
+            #2b. compare it with h
+            #2c. move the pointers left or right
+        #3. check the pointers and return result
+
+        left = 1
+        right = max(piles)
 
         while left + 1 < right:
             mid = left + (right - left) // 2  
-
-            # Calculate total hours needed at speed 'mid'
-            hours = 0
+            
+            est = 0
             for p in piles:
-                hours += ceil(p/mid)
+                est += ceil(p / mid) 
 
-            if hours <= h:
+            if est <= h:
                 right = mid
             else:
                 left = mid
 
-        time_left = sum(ceil(p / left) for p in piles)
-        if time_left <= h:
+        est = sum(ceil(p / left) for p in piles)
+        if est <= h:
             return left
         return right
+
