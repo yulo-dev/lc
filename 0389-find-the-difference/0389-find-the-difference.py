5ca407d49b0ba21b77cab7809f1a02b18bdc3fd4
Yulo L. <loyu.jobs@gmail.com>
2025-12-05 23:31:41 -0800

    Time: 2 ms (59.33%), Space: 18 MB (23.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -3,11 +3,8 @@ class Solution:
         s_dict = Counter(s)
         t_dict = Counter(t)
 
-        if s == "":
-            return t
-
-        for t in t_dict:
-            if t not in s_dict:
-                return t
-            elif t_dict[t] != s_dict[t]:
-                return t
+        for ch in t_dict:
+            if ch not in s_dict:
+                return ch
+            elif t_dict[ch] != s_dict[ch]:
+                return ch
