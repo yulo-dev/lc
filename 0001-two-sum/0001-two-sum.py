a34f634c89e29132407a8293408bf9d229a2cea3
Yulo L. <loyu.jobs@gmail.com>
2025-12-05 23:15:31 -0800

    Time: 0 ms (100%), Space: 19 MB (34.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap ={}
-
-        for i, num in enumerate(nums):
-            comp = target - num
+        hashmap = {}
+        for i, val in enumerate(nums):
+            comp = target - val
             if comp in hashmap:
                 return [hashmap[comp], i]
-            hashmap[num] = i
+            hashmap[val] = i
