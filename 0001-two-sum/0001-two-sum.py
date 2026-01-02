16411feb31de2639d26b6f9d265e09082148cf91
Yulo L. <loyu.jobs@gmail.com>
2026-01-01 17:40:21 -0800

    Time: 0 ms (100%), Space: 18.6 MB (66.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        mapping = {}
+        # mapping: {key: value}; key = target - current value; value = index
+        # for loop to iterate through the nums
+        # if value in mapping: append result
+        # if not, store the info in mapping 
 
+        mapping = {}
+        
         for i, val in enumerate(nums):
             comp = target - val
             if comp in mapping:
                 return [mapping[comp], i]
             
             mapping[val] = i
+        
+        return res
