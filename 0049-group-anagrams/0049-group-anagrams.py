27a126dfd95a52ee57e71e0d55c3d77964f2955d
Yulo L. <loyu.jobs@gmail.com>
2026-01-06 05:55:21 -0800

    Time: 11 ms (82.85%), Space: 22.2 MB (26.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
         mapping = defaultdict(list)
 
         for s in strs:
-            keys = "".join(sorted(s))
+            keys = ''.join(sorted(s))
             mapping[keys].append(s)
 
         return list(mapping.values())
