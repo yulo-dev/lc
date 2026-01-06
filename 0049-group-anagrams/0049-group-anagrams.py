451135beaedc512a625ea3aafc84330ad71b3b2b
Yulo L. <loyu.jobs@gmail.com>
2026-01-06 06:27:34 -0800

    Time: 7 ms (97.93%), Space: 22.1 MB (27.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,7 +3,7 @@ class Solution:
         mapping = defaultdict(list)
 
         for s in strs:
-            keys = ''.join(sorted(s))
+            keys = ''.join(sorted(s)) 
             mapping[keys].append(s)
 
         return list(mapping.values())
