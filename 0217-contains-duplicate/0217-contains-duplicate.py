2389c5319ed115a35403a168dbbcabd6afa6c0f5
Yulo L. <loyu.jobs@gmail.com>
2026-01-06 00:39:09 -0800

    Time: 2 ms (98.84%), Space: 31.3 MB (69.85%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,10 +1,5 @@
-from collections import defaultdict
-
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        freq = {}
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
-            if freq[num] > 1:
-                return True
-        return False
+        uni = set(nums)
+
+        return len(nums) != len(uni)
