31f554d339a6d83086d1d2aa5b426325191e0f91
Yulo L. <loyu.jobs@gmail.com>
2026-01-06 19:25:21 -0800

    Time: 16 ms (93.49%), Space: 25.1 MB (32.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -8,7 +8,7 @@ class Solution:
             prefix *= nums[i]
 
         postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums) -1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
 
