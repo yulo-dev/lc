099be6bf10125401cbc78c61b9a1fa926af88b86
Yulo L. <loyu.jobs@gmail.com>
2026-01-08 00:46:47 -0800

    Time: 4 ms (70.94%), Space: 21.1 MB (16.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,8 +4,7 @@ class Solution:
         count = 0
 
         for n in nums:
-            if count == 0:
+            if not candidate or count == 0:
                 candidate = n
             count += 1 if n == candidate else -1
-
         return candidate
