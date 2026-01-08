e14e35da27fbcc30c7af3549d3819dc51f322132
Yulo L. <loyu.jobs@gmail.com>
2026-01-08 01:02:48 -0800

    Time: 11 ms (59.08%), Space: 19.9 MB (13.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -8,11 +8,11 @@ class Solution:
                 return
             if i >= len(candidates) or total > target:
                 return
-
+            
             cur.append(candidates[i])
             dfs(i, cur, total + candidates[i])
             cur.pop()
             dfs(i + 1, cur, total)
-        
+
         dfs(0, [], 0)
         return res
