a81681308767f8f61f128091a9ce9ec244cf87f5
Yulo L. <loyu.jobs@gmail.com>
2026-01-08 11:25:31 -0800

    Time: 4 ms (14.28%), Space: 19.6 MB (18.3%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,15 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        
         cnt = Counter(s)
         total = 0
         odd = False
 
         for freq in cnt.values():
             total += (freq // 2) * 2
-            if (freq % 2) == 1:
+            if freq % 2 == 1:
                 odd = True
-        
+
         if odd:
             total += 1
 
