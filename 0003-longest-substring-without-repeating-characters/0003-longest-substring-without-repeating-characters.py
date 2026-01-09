e406e46436939e1a1fe9e3d23a5044501849fc4c
Yulo L. <loyu.jobs@gmail.com>
2026-01-09 09:22:11 -0800

    Time: 11 ms (94.54%), Space: 19.3 MB (10.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,8 +7,8 @@ class Solution:
         for right, val in enumerate(s):
             if val in seen and seen[val] >= left:
                 left = seen[val] + 1
-
+            
             seen[val] = right
             res = max(res, right - left + 1)
-
+        
         return res
