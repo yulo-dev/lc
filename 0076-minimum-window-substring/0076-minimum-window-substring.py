a4add2ff1d4081495518b3f2d2ca63eabeb26df4
Yulo L. <loyu.jobs@gmail.com>
2026-01-09 09:11:14 -0800

    Time: 65 ms (72.33%), Space: 19.9 MB (5.01%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t:
+            return ""
+
+        t_cnt = Counter(t)
+        window = defaultdict(int)
+
+        formed = 0
+
+        left = 0 
+        best_len = float("inf")
+        best_l = 0
+
+        for right, val in enumerate(s):
+            window[val] += 1
+            if val in t_cnt and window[val] == t_cnt[val]:
+                formed += 1
+            
+            while formed == len(t_cnt):
+                if (right - left + 1) < best_len:
+                    best_len = right - left + 1
+                    best_l = left
+
+                window[s[left]] -= 1
+                if s[left] in t_cnt and window[s[left]] < t_cnt[s[left]]:
+                    formed -= 1
+                left += 1
+
+        if best_len == float("inf"):
+            return ""
+        else:
+            return s[best_l:best_l + best_len] 
