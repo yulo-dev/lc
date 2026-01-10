34bdf3b32d91f73b953971308736d2f9c34a2f7d
Yulo L. <loyu.jobs@gmail.com>
2026-01-10 09:04:58 -0800

    Time: 19 ms (86.53%), Space: 25.6 MB (29.9%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,12 +4,12 @@ class Solution:
         res = [0] * len(nums)
 
         prefix = 1
-        for i in range(len(nums)):
+        for i, num in enumerate(nums):
             res[i] = prefix
             prefix *= nums[i]
 
         postfix = 1
-        for i in range(len(nums) -1, -1, -1):
+        for i in range(len(nums) - 1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
 
