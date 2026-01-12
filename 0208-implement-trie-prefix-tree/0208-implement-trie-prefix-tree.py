7f17d99eb2e84a711198d674997d6dfa50767432
Yulo L. <loyu.jobs@gmail.com>
2026-01-12 10:00:41 -0800

    Time: 35 ms (84.23%), Space: 34.2 MB (16.34%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,40 +1,34 @@
-class TrieNode:
+class TreeNode:
     def __init__(self):
         self.children = {}
         self.is_end = False
 
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
+       self.root = TreeNode()
 
     def insert(self, word: str) -> None:
         cur = self.root
-
         for ch in word:
             if ch not in cur.children:
-                cur.children[ch] = TrieNode() #幫字母ch這條路建立下一個節點, 字母ch已經在dict的key裡, 所以節點不用再存一次字母
-            cur = cur.children[ch] #我用字母ch當索引，去拿到那個「下一層節點」
-
+                cur.children[ch] = TreeNode()
+            cur = cur.children[ch]
         cur.is_end = True
-
+       
     def search(self, word: str) -> bool:
         cur = self.root
-
         for ch in word:
             if ch not in cur.children:
                 return False
             cur = cur.children[ch]
-
         return cur.is_end
-        
+      
     def startsWith(self, prefix: str) -> bool:
         cur = self.root
-
         for ch in prefix:
             if ch not in cur.children:
                 return False
             cur = cur.children[ch]
-        
         return True
         
 # Your Trie object will be instantiated and called as such:
