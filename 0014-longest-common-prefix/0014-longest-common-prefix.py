054341d03e43582af1a6038f29b323a61b2290f0
Yulo L. <loyu.jobs@gmail.com>
2026-01-11 23:50:17 -0800

    Time: 3 ms (27.76%), Space: 19.4 MB (6.83%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,15 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs: 
-            return ""
-
         res = ""
 
-        for i in range(len(strs[0])): #把 strs[0] 當作基準，因為共同前綴不可能比它更長
+        for i in range(len(strs[0])):
             for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
+                if i == len(s) or strs[0][i] != s[i]:
                     return res
-            
+        
             res += strs[0][i]
 
         return res
