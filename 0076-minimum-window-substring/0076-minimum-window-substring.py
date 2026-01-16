159305d2b33947688e0bd8fc21ce20af7246282a
Yulo L. <loyu.jobs@gmail.com>
2026-01-15 20:44:15 -0800

    Time: 87 ms (44.81%), Space: 20 MB (8.07%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -24,12 +24,12 @@ class Solution:
                     best_len = right - left + 1
                     best_left = left
 
-                window[s[left]] -= 1
+                window[s[left]] -= 1 #1)移掉它
 
                 if s[left] in t_cnt and window[s[left]] < t_cnt[s[left]]:
-                    formed -= 1
+                    formed -= 1  #2)如果跌破才 formed--
 
-                left += 1
+                left += 1   #3)left 一定要前進
 
         if best_len == float("inf"):
             return ""
