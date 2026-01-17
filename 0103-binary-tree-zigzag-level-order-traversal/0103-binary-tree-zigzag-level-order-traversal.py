358f90110c9022499383fdf0d33e99799d11697f
Yulo L. <loyu.jobs@gmail.com>
2026-01-17 00:32:38 -0800

    Time: 0 ms (100%), Space: 19.5 MB (9.21%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -17,19 +17,20 @@ class Solution:
         REVERSE = False
 
         while queue:
-            level = []
+            level = deque()
             for _ in range(len(queue)):
                 node = queue.popleft()
-                level.append(node.val)
+                if not REVERSE:
+                    level.append(node.val)
+                else:
+                    level.appendleft(node.val)
+
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
 
-            if REVERSE:
-                level.reverse() #他需要先寫出來再append是因爲reverse()是in-place 所以如果寫 res.append(level.reverse()) 等同於 res.append(None)
-            
-            res.append(level)
+            res.append(list(level))
         
             REVERSE = not REVERSE
 
