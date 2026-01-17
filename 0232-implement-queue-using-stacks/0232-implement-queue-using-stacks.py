cc3fa3b0ad1a8393dc8ce40ed76e84b7bd7d5792
Yulo L. <loyu.jobs@gmail.com>
2026-01-16 16:00:33 -0800

    Time: 0 ms (100%), Space: 19.6 MB (10.15%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -6,22 +6,22 @@ class MyQueue:
 
     def push(self, x: int) -> None:
         self.in_stack.append(x)
-
+    
     def _move(self):
         if not self.out_stack:
             while self.in_stack:
                 self.out_stack.append(self.in_stack.pop())
-    
+
     #拿出「隊首元素」，而且會移除
     def pop(self) -> int:
         self._move()
         return self.out_stack.pop()
-
+       
     #看「隊首元素」是誰，但不移除
     def peek(self) -> int:
         self._move()
         return self.out_stack[-1]
-
+       
     def empty(self) -> bool:
         return not self.in_stack and not self.out_stack
 
