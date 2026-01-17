21b03ca2cb03a66258de8fa9be89e9ff59e9d643
Yulo L. <loyu.jobs@gmail.com>
2026-01-17 09:52:47 -0800

    Time: 0 ms (100%), Space: 21 MB (9%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -13,7 +13,7 @@ class Solution:
 
             if not (low < node.val < high):
                 return False
-
+            
             return (validate(node.left, low, node.val) and validate(node.right, node.val, high))
 
         return validate(root, float("-inf"), float("inf"))
