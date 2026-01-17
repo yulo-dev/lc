badced3b7f9ad8243cd7462e8659f22f6b3061dd
Yulo L. <loyu.jobs@gmail.com>
2026-01-17 00:06:47 -0800

    Time: 3 ms (19.89%), Space: 20.1 MB (5.13%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -20,12 +20,11 @@ class Solution:
             for _ in range(len(queue)):
                 node = queue.popleft()
                 level.append(node.val)
-
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-
+            
             res.append(level)
 
         return res
