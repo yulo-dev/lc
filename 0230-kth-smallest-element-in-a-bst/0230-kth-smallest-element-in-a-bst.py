053b62778df2051e70dcae08af03e87da39bf5a7
Yulo L. <loyu.jobs@gmail.com>
2026-01-16 21:20:01 -0800

    Time: 0 ms (100%), Space: 22.2 MB (15.46%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -7,22 +7,23 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         self.cnt = 0
-        self.res = None
 
         def inorder(node):
             if not node:
                 return
 
-            inorder(node.left)
+            left = inorder(node.left)
 
-            if node:
-                self.cnt += 1
+            if left is not None:
+                return left # 1) 左邊有答案就直接回傳
+
+            # 2) 左邊沒答案，才處理自己
+            self.cnt += 1
             
             if self.cnt == k:
-                self.res = node.val
+                return node.val
 
-            inorder(node.right)
+            # 3) 自己也不是答案，才去右邊
+            return inorder(node.right)
 
-        inorder(root)
-
-        return self.res
+        return inorder(root)
