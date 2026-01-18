cec7d55350618d2a8aeadbd3a6123384f1cb34ad
Yulo L. <loyu.jobs@gmail.com>
2026-01-17 16:54:09 -0800

    Time: 0 ms (100%), Space: 19.5 MB (5.2%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -10,15 +10,13 @@ class Solution:
             return None
 
         queue = deque([root])
-  
+
         while queue:
             node = queue.popleft()
-
             node.left, node.right = node.right, node.left
-            
             if node.left:
                 queue.append(node.left)
             if node.right:
-                 queue.append(node.right)
-
+                queue.append(node.right)
+        
         return root
