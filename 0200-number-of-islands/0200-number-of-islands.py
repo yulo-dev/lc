27397559e5dde3ae933acf4f68d1d397a3529b2c
Yulo L. <loyu.jobs@gmail.com>
2026-01-17 21:35:01 -0800

    Time: 278 ms (18.4%), Space: 26.9 MB (14.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -11,32 +11,31 @@ class Solution:
         for x in range(len(grid)):
             for y in range(len(grid[0])):
                 if (x, y) not in visited and grid[x][y] == "1":
-                    self.bfs(grid, x, y, visited)
+                    self.bfs(grid, x, y, visited)  
                     island += 1
         return island
 
     def bfs(self, grid, x, y, visited):
-        DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
-        queue = deque([(x,y)])
+        DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
+        queue = deque([(x, y)])
         visited.add((x,y))
 
         while queue:
             x, y = queue.popleft()
             for dir_x, dir_y in DIRECTIONS:
-                new_x = dir_x + x
-                new_y = dir_y + y
+                new_x = x + dir_x
+                new_y = y + dir_y
                 if not self.is_valid(grid, new_x, new_y, visited):
                     continue
-                queue.append((new_x, new_y))
                 visited.add((new_x, new_y))
-        
+                queue.append((new_x, new_y))
+    
     def is_valid(self, grid, x, y, visited):
         m = len(grid)
         n = len(grid[0])
 
-        if (x, y) in visited:
+        if not (0 <= x < m and 0<= y < n):
             return False
-        if not (0 <= x < m) or not (0 <= y < n):
+        if (x,y) in visited:
             return False
-
         return grid[x][y] == "1"
