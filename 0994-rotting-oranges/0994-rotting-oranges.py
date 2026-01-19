1c830e640c7f3cee39755d0abb2e694e3ac6b280
Yulo L. <loyu.jobs@gmail.com>
2026-01-19 13:39:27 -0800

    Time: 4 ms (43.42%), Space: 19.5 MB (10.14%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -9,32 +9,31 @@ class Solution:
         queue = deque([])
         visited = set()
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                val = grid[i][j]
-                if val == 2:
-                    queue.append((i,j,0))
-                    visited.add((i,j))
-                elif val == 1:
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] == 2:
+                    queue.append((x,y,0))
+                    visited.add((x,y))
+                elif grid[x][y] == 1:
                     fresh += 1
 
         minutes, fresh = self.bfs(grid, queue, fresh, visited)
         return minutes if fresh == 0 else -1
 
     def bfs(self, grid, queue, fresh, visited):
-        DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
+        DIRECTOINS = [(1,0), (-1,0), (0,1), (0,-1)]
         minutes = 0
-        
+
         while queue:
-            x, y, minute = queue.popleft()
+            x,y,minute = queue.popleft()
             minutes = max(minute, minutes)
-            for dir_x, dir_y in DIRECTIONS:
-                new_x = dir_x + x
-                new_y = dir_y + y
+            for dir_x, dir_y in DIRECTOINS:
+                new_x = x + dir_x
+                new_y = y + dir_y
 
                 if not self.is_valid(grid, new_x, new_y, visited):
                     continue
-                
+
                 fresh -= 1
                 queue.append((new_x, new_y, minute + 1))
                 visited.add((new_x, new_y))
@@ -45,11 +44,8 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        if not (0 <= x < m) or not (0 <= y < n):
+        if not (0 <= x < m and 0 <= y < n):
             return False
-        if (x, y) in visited:
+        if (x,y) in visited:
             return False
-
         return grid[x][y] == 1
-            
-
