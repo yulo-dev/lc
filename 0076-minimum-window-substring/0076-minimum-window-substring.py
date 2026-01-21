bb9ddd94e6f7e9d02617a425b291dcbf6f395ddb
Yulo L. <loyu.jobs@gmail.com>
2026-01-20 17:56:44 -0800

    Time: 87 ms (44.5%), Space: 19.8 MB (11.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,8 +1,5 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t:
-            return ""
-
         t_cnt = Counter(t)
         window = Counter()
 
@@ -11,27 +8,22 @@ class Solution:
         left = 0
         best_len = float("inf")
         best_left = 0
-
+        
         for right, val in enumerate(s):
             window[val] += 1
-
             if val in t_cnt and window[val] == t_cnt[val]:
                 formed += 1
-
             while formed == len(t_cnt):
-
                 if (right - left + 1) < best_len:
                     best_len = right - left + 1
                     best_left = left
 
                 window[s[left]] -= 1
-
                 if s[left] in t_cnt and window[s[left]] < t_cnt[s[left]]:
                     formed -= 1
-
                 left += 1
-        
+
         if best_len == float("inf"):
             return ""
         else:
-            return s[best_left:best_left+best_len]
+            return s[best_left:best_len+best_left]
