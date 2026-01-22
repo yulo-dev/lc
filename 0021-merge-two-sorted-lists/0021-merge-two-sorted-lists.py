cbe244aaf37d7775f386ee844262d6869d24204f
Yulo L. <loyu.jobs@gmail.com>
2026-01-22 12:23:37 -0800

    Time: 0 ms (100%), Space: 19.5 MB (10.88%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,6 +6,7 @@
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
         dummy = ListNode()
         curr = dummy
 
