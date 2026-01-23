a3a25725df8801039195b666286805965b7ba425
Yulo L. <loyu.jobs@gmail.com>
2026-01-22 21:46:54 -0800

    Time: 0 ms (100%), Space: 19.3 MB (29.47%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -9,26 +9,23 @@ class Solution:
         if not head or not head.next or k == 0:
             return head
 
-        #length + move to tail:
         length = 1
         curr = head
-        while curr.next:  #為了避免curr走到None 這樣之後沒辦法直接接說 curr.next = head 去做cycle
+        while curr.next:
             curr = curr.next
             length += 1
 
-        #count rotate
         k = k % length
         if k == 0:
             return head
 
-        #make a cycle
+        # create cycle
         curr.next = head
 
-        #new tail & new head
+        #create new tail & head
         new_tail = head
         for _ in range(length - k - 1):
             new_tail = new_tail.next
-
         new_head = new_tail.next
 
         #break cycle
