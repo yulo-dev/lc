38e3f668e5072a52d5d3b6ae2e3ade9b064c897a
Yulo L. <loyu.jobs@gmail.com>
2026-01-22 22:42:57 -0800

    Time: 2 ms (28.37%), Space: 19.5 MB (9.66%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -7,12 +7,13 @@ class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
         curr = head
-
         while curr and curr.next:
+            #如果下一顆跟它一樣，就刪掉下一顆 但curr先不動
             if curr.val == curr.next.val:
                 curr.next = curr.next.next
+
+            #直到下一顆不同了，才代表這個值清完了，curr 才能往前走
             else:
                 curr = curr.next
 
         return head
-        
