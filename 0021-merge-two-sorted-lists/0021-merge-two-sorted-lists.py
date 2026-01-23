64cac483964a8e4275234ee57495873ef2965c04
Yulo L. <loyu.jobs@gmail.com>
2026-01-22 21:06:05 -0800

    Time: 0 ms (100%), Space: 19.6 MB (10.88%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -17,9 +17,9 @@ class Solution:
             else:
                 curr.next = list2
                 list2 = list2.next
-            
-            curr = curr.next
 
+            curr = curr.next
+        
         if list1:
             curr.next = list1
         if list2:
