c496b4c554600a943cebfcf0761143d4edc975b4
Yulo L. <loyu.jobs@gmail.com>
2026-01-24 21:34:29 -0800

    Time: 283 ms (14.85%), Space: 26.9 MB (15.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,31 +5,35 @@ class Solution:
         if not grid or not grid[0]:
             return 0
 
-        visited = set()
         island = 0
+        visited = set()
 
         for x in range(len(grid)):
             for y in range(len(grid[0])):
                 if (x,y) not in visited and grid[x][y] == "1":
                     self.bfs(grid, x, y, visited)
                     island += 1
+
         return island
 
     def bfs(self, grid, x, y, visited):
-        DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
+
         queue = deque([(x,y)])
         visited.add((x,y))
+        DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
 
         while queue:
             x, y = queue.popleft()
             for dir_x, dir_y in DIRECTIONS:
                 new_x = x + dir_x
                 new_y = y + dir_y
+
                 if not self.is_valid(grid, new_x, new_y, visited):
                     continue
+
                 queue.append((new_x, new_y))
                 visited.add((new_x, new_y))
-    
+
     def is_valid(self, grid, x, y, visited):
         m = len(grid)
         n = len(grid[0])
