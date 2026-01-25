508f89781ed8572942e00a083e1df1320cdc4b68
Yulo L. <loyu.jobs@gmail.com>
2026-01-24 21:19:29 -0800

    Time: 1 ms (24.36%), Space: 19.5 MB (27.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,32 +1,34 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
         orig_color = image[sr][sc]
         if orig_color == color:
             return image
 
         visited = set()
 
-        #Flood Fill 只需要從 (sr, sc) 的連通區域開始擴散，不是把所有 orig_color 的點都 fill
         self.bfs(image, sr, sc, visited, color, orig_color)
 
         return image
 
-    def bfs(self, image, x, y, visited, color, orig_color):
-        DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
-        queue = deque([(x,y)])
-        visited.add((x,y))
+    def bfs(self, image, sr, sc, visited, color, orig_color):
+        DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
+        queue = deque([(sr,sc)])
+        visited.add((sr,sc))
 
         while queue:
             x, y = queue.popleft()
-            image[x][y] = color #出 queue 就染一次
+            image[x][y] = color
             for dir_x, dir_y in DIRECTIONS:
                 new_x = x + dir_x
                 new_y = y + dir_y
+
                 if not self.is_valid(image, new_x, new_y, visited, orig_color):
                     continue
+
                 queue.append((new_x, new_y))
                 visited.add((new_x, new_y))
-    
+
     def is_valid(self, image, x, y, visited, orig_color):
         m = len(image)
         n = len(image[0])
@@ -35,4 +37,5 @@ class Solution:
             return False
         if (x,y) in visited:
             return False
+
         return image[x][y] == orig_color
