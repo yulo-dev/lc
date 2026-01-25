0aa00de96006a26dc6acb64dd90ff5e828e28c51
Yulo L. <loyu.jobs@gmail.com>
2026-01-24 23:24:58 -0800

    Time: 195 ms (22.05%), Space: 23.6 MB (5.28%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,47 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        if not mat or not mat[0]:
+            return mat
+
+        m = len(mat)
+        n = len(mat[0])
+        res = [[0] * n for _ in range(m)]
+        queue = deque([])
+        visited = set()
+
+        for x in range(len(mat)):
+            for y in range(len(mat[0])):
+                if mat[x][y] == 0:
+                    queue.append((x,y,0))
+                    visited.add((x,y))
+                    res[x][y] = 0
+        
+        res = self.bfs(mat, queue, visited,res)
+        return res
+
+    def bfs(self, mat, queue, visited, res):
+        DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
+        
+        while queue:
+            x, y, dist = queue.popleft()
+            for dir_x, dir_y in DIRECTIONS:
+                new_x = x + dir_x
+                new_y = y + dir_y
+
+                if not self.is_valid(mat, new_x, new_y, visited):
+                    continue
+                
+                res[new_x][new_y] = dist + 1
+                queue.append((new_x, new_y, dist + 1))
+                visited.add((new_x, new_y))
+
+        return res
+
+    def is_valid(self, mat, x, y, visited):
+        m = len(mat)
+        n = len(mat[0])
+        if not (0 <= x < m and 0 <= y < n):
+            return False
+        if (x, y) in visited:
+            return False
+        return True
