ce86696fed7e1904c77da4b0f2ac4c1b0cb5ee34
Yulo L. <loyu.jobs@gmail.com>
2026-01-26 11:43:48 -0800

    Time: 0 ms (100%), Space: 19.2 MB (37.63%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -5,11 +5,14 @@ class Solution:
 
         while i < len(nums):
             start = nums[i]
-            while i + 1 < len(nums) and nums[i] + 1 == nums[i+1]:
+            while i + 1 < len(nums) and nums[i] + 1 == nums[i + 1]:
                 i += 1
+
             if start != nums[i]:
                 res.append(str(start) + "->" + str(nums[i]))
             else:
                 res.append(str(nums[i]))
+                
             i += 1
+
         return res
