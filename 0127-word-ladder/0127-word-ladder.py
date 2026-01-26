e2f4cbbac964a12f7e144477145622a87b6def90
Yulo L. <loyu.jobs@gmail.com>
2026-01-25 23:25:40 -0800

    Time: 213 ms (51.46%), Space: 20.7 MB (50.69%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -11,30 +11,27 @@ class Solution:
         distance = 1
 
         while queue:
-            for _ in range(len(queue)):  # 同一層（同一步數）
+            for _ in range(len(queue)):
                 word = queue.popleft()
                 if word == endWord:
                     return distance
-
-                for new_word in self.get_next_word(word):  # 產生所有「差一個字母」候選
+                for new_word in self.find_new_word(word):
                     if new_word not in wordset or new_word in visited:
                         continue
                     queue.append(new_word)
                     visited.add(new_word)
+
             distance += 1
 
         return 0
 
-
-    #產生所有「只改一個字母」的候選單字
-    def get_next_word(self, word):
+    def find_new_word(self, word):
         words = []
-
         for i in range(len(word)):
             left = word[:i]
-            right = word[i + 1:]
+            right = word[i+1:]
             for ch in 'abcdefghijklmnopqrstuvwxyz':
-                if ch == word[i]:
+                if word[i] == ch:
                     continue
                 words.append(left + ch + right)
                 
