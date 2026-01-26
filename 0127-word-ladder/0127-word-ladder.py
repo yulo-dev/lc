54c83bb0f26d9ae6940f863811b4b58475e33e40
Yulo L. <loyu.jobs@gmail.com>
2026-01-25 21:49:47 -0800

    Time: 219 ms (51.15%), Space: 20.4 MB (63.98%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,37 +2,39 @@ from collections import deque
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: list[str]) -> int:
-        wordSet = set(wordList)
-        if endWord not in wordSet:
+        wordset = set(wordList)
+        if endWord not in wordset:
             return 0
-        
+
         queue = deque([beginWord])
         visited = set([beginWord])
         distance = 1
 
         while queue:
-            for _ in range(len(queue)):
+            for _ in range(len(queue)):  # 同一層（同一步數）
                 word = queue.popleft()
                 if word == endWord:
-                    return distance 
-                
-                for next_word in self.choose_word(word):
-                    if next_word in wordSet and next_word not in visited:
-                        queue.append(next_word)
-                        visited.add(next_word)
-            
+                    return distance
+
+                for new_word in self.get_next_word(word):  # 產生所有「差一個字母」候選
+                    if new_word not in wordset or new_word in visited:
+                        continue
+                    queue.append(new_word)
+                    visited.add(new_word)
             distance += 1
-        
+
         return 0
 
-    def choose_word(self, word):
+
+    #產生所有「只改一個字母」的候選單字
+    def get_next_word(self, word):
         words = []
+
         for i in range(len(word)):
             left = word[:i]
-            right = word[i+1:]
-
+            right = word[i + 1:]
             for ch in 'abcdefghijklmnopqrstuvwxyz':
-                if word[i] == ch:
+                if ch == word[i]:
                     continue
                 words.append(left + ch + right)
                 
