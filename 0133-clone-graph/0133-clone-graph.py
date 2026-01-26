fef3028344f780f32b9dc8278571aab0789a2253
Yulo L. <loyu.jobs@gmail.com>
2026-01-25 16:25:56 -0800

    Time: 48 ms (37.15%), Space: 19.9 MB (7.14%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -12,18 +12,16 @@ class Solution:
         if not node:
             return None
 
-        oldToNew = {} #key是原來的node, value是複製出來的new node
-        oldToNew[node] = Node(node.val) #原節點 node → 複製節點 copy(node)
-        queue = deque([node]) #放入起點
+        oldtonew = {}
+        oldtonew[node] = Node(node.val)
+        queue = deque([node])
 
         while queue:
-            cur = queue.popleft()
-            for nei in cur.neighbors:
-                #(1)每拿到一個 nei（原圖鄰居），你先確保它有 clone（必要時建立）
-                if nei not in oldToNew:
-                    oldToNew[nei] = Node(nei.val)   # “標記 visited” + “建立 clone”
+            curr = queue.popleft()
+            for nei in curr.neighbors:
+                if nei not in oldtonew:
+                    oldtonew[nei] = Node(nei.val)
                     queue.append(nei)
-                #(2)最後把 clone(nei) 加進 clone(cur) 的 neighbors
-                oldToNew[cur].neighbors.append(oldToNew[nei])  # 在「新圖」上建立同樣的邊
+                oldtonew[curr].neighbors.append(oldtonew[nei])
 
-        return oldToNew[node]
+        return oldtonew[node]
