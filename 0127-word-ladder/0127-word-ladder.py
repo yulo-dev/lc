0edb9880825747ccb0416165f2969ba9a9454bb9
Yulo L. <loyu.jobs@gmail.com>
2026-01-26 17:08:38 -0800

    Time: 215 ms (51.67%), Space: 20.6 MB (57.37%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -3,7 +3,7 @@ from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: list[str]) -> int:
         wordset = set(wordList)
-        if endWord not in wordset:
+        if endWord not in wordList:
             return 0
 
         queue = deque([beginWord])
@@ -11,28 +11,29 @@ class Solution:
         distance = 1
 
         while queue:
-            for _ in range(len(queue)):
+            for _ in range(len(queue)): #同一個步數的字一起處理
                 word = queue.popleft()
                 if word == endWord:
                     return distance
-                for new_word in self.find_new_word(word):
+
+                for new_word in self.valid_word(word):
                     if new_word not in wordset or new_word in visited:
                         continue
+
                     queue.append(new_word)
                     visited.add(new_word)
-
-            distance += 1
+            distance += 1 #同個步數一起更新一次長度
 
         return 0
 
-    def find_new_word(self, word):
+    def valid_word(self, word):
         words = []
         for i in range(len(word)):
             left = word[:i]
-            right = word[i+1:]
+            right = word[i + 1:]
             for ch in 'abcdefghijklmnopqrstuvwxyz':
-                if word[i] == ch:
+                if ch == word[i]:
                     continue
                 words.append(left + ch + right)
-                
+
         return words
