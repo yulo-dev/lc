4926511e21a186291d4193b03e3ba59e5c402e68
Yulo L. <loyu.jobs@gmail.com>
2026-01-27 17:38:39 -0800

    Time: 333 ms (29.52%), Space: 20.6 MB (50.84%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,7 +2,14 @@ from collections import deque
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: list[str]) -> int:
+
+        #I convert wordList into a set so I can validate neighbors in constant time, because during BFS I’ll generate 
+        #many candidate words and I need to quickly test whether a candidate exists in the dictionary.
+        #If I kept it as a list, the in check would be O(N) each time, which would be too slow given we generate 
+        #up to 26·L neighbors per word.
+        
         wordset = set(wordList)
+
         if endWord not in wordset:
             return 0
 
@@ -11,7 +18,7 @@ class Solution:
         visited = set([beginWord])
 
         while queue:
-            for i in range(len(queue)):
+            for _ in range(len(queue)): # process one BFS level
                  word = queue.popleft()
                  if word == endWord:
                     return dist
@@ -20,10 +27,13 @@ class Solution:
                         continue
                     queue.append(next_word)
                     visited.add(next_word)
-            dist += 1
+            dist += 1 # each level, dist + 1
 
         return 0
-    
+
+
+    #For a word, I generate all its neighbors by changing each character position to a..z (except the original char). 
+    #If the generated word exists in wordSet and hasn’t been visited, I push it into the queue.
     def find_word(self, word):
         words = []
 
