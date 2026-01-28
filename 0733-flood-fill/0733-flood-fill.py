9832b2100f4a0bdc40a20ea822616a17359f34a6
Yulo L. <loyu.jobs@gmail.com>
2026-01-27 16:07:58 -0800

    Time: 0 ms (100%), Space: 19.6 MB (16.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,21 +1,19 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
         orig_color = image[sr][sc]
         if orig_color == color:
             return image
 
         visited = set()
 
-        self.bfs(image, sr, sc, orig_color, color, visited)
+        self.bfs(image, sr, sc, visited, orig_color, color)
 
         return image
 
-    def bfs(self, image, sr, sc, orig_color, color, visited):
-        DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
-
+    def bfs(self, image, sr, sc, visited, orig_color, color):
+        DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
         queue = deque([(sr, sc)])
-        visited.add((sr, sc))
+        visited.add((sr,sc))
 
         while queue:
             x, y = queue.popleft()
@@ -23,9 +21,10 @@ class Solution:
             for dir_x, dir_y in DIRECTIONS:
                 new_x = x + dir_x
                 new_y = y + dir_y
+
                 if not self.is_valid(image, new_x, new_y, visited, orig_color):
                     continue
-
+                
                 queue.append((new_x, new_y))
                 visited.add((new_x, new_y))
 
@@ -35,7 +34,6 @@ class Solution:
 
         if not (0 <= x < m and 0 <= y < n):
             return False
-
         if (x,y) in visited:
             return False
 
