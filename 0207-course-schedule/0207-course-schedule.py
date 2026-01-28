38a6e2bf9776db3eff6b57b0b3a31bfd6921bff8
Yulo L. <loyu.jobs@gmail.com>
2026-01-27 19:19:12 -0800

    Time: 5 ms (54.95%), Space: 20.6 MB (27.57%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,13 +1,14 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for i in range(numCourses)]
-        in_degree = [0] * numCourses
+        graph = [[] for i in range(numCourses)] #代表類似 graph[0]：從課程 0 出發可以到哪些課
+        in_degree = [0] * numCourses # in_degree[i] = 有多少門先修課指向 i（i 還缺幾個 prerequisite）
         
         #建圖
         # build graph + indegree
-        for node_in, node_out in prerequisites:
-            graph[node_out].append(node_in)
-            in_degree[node_in] += 1
+        #[course, prereq] means prereq -> course
+        for course, prereq in prerequisites:
+            graph[prereq].append(course) 
+            in_degree[course] += 1
         
         queue = deque()
         
@@ -19,13 +20,14 @@ class Solution:
         #queue = deque([i for i in range(n) if in_degree[i] == 0])
         
         num_choose = 0
-        while queue:
-            now_pos = queue.popleft()
-            num_choose += 1
-            # 將鄰邊刪掉，如果indegree=0，再加入queue
-            for next_pos in graph[now_pos]:
-                in_degree[next_pos] -= 1
-                if in_degree[next_pos] == 0:
-                    queue.append(next_pos)
+        while queue: #只要還有可以修的課，就繼續修
+            course = queue.popleft() #拿出一門「現在可修」的課，準備把它修掉
+            num_choose += 1 #計數：我已經成功修完一門課
+
+            # 看「修完 course 之後，哪些課會被解鎖/受影響」，如果indegree=0，再加入queue
+            for next_course in graph[course]:
+                in_degree[next_course] -= 1
+                if in_degree[next_course] == 0:
+                    queue.append(next_course)
         
         return num_choose == numCourses
