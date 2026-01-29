2e2e90bbe347bfe91d2725abc73da344b354a7d8
Yulo L. <loyu.jobs@gmail.com>
2026-01-28 16:41:07 -0800

    Time: 118 ms (71.92%), Space: 78.9 MB (6.94%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -2,7 +2,6 @@ class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        #DLL
         self.prev = None
         self.next = None
 
@@ -15,24 +14,24 @@ class LRUCache:
         self.tail = Node(0,0)
         self.head.next = self.tail
         self.tail.prev = self.head
-
+        
     def _remove(self, node):
         prev = node.prev
         nxt = node.next
         prev.next = nxt
         nxt.prev = prev
 
-    def _add_to_start(self, node):
+    def _add_to_front(self, node):
         first = self.head.next
         node.next = first
         node.prev = self.head
         self.head.next = node
         first.prev = node
 
-    def _move_to_start(self, node):
+    def _move_to_front(self, node):
         self._remove(node)
-        self._add_to_start(node)
-    
+        self._add_to_front(node)
+
     def _lru_remove(self):
         lru = self.tail.prev
         self._remove(lru)
@@ -42,25 +41,24 @@ class LRUCache:
         if key not in self.map:
             return -1
         node = self.map[key]
-        self._move_to_start(node)
+        self._move_to_front(node)
         return node.val
 
     def put(self, key, val):
         if key in self.map:
-            node = self.map[key] 
+            node = self.map[key]
             node.val = val
-            self._move_to_start(node)
-            return
-
+            self._move_to_front(node)
+            return 
         node = Node(key, val)
         self.map[key] = node
-        self._add_to_start(node)
+        self._add_to_front(node)
 
         if len(self.map) > self.cap:
             lru = self._lru_remove()
             del self.map[lru.key]
 
-      
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
