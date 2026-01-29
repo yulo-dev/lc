ab0b4cb264acc87f43a9c5b514176fc2ecd84047
Yulo L. <loyu.jobs@gmail.com>
2026-01-28 23:32:06 -0800

    Time: 0 ms (100%), Space: 19.5 MB (15.85%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -9,5 +9,8 @@ class Solution:
 
             if y != x:
                 heapq.heappush(h, -(y - x))
+
+            #不用特別寫兩顆石頭相同時的情況 因為這情況下兩顆石頭應該要被砸碎然後消失
+            #所以就是他們都被pop出來然後不處理 也不push回去 下一輪也不會再出現 就代表消失了
         
         return -h[0] if h else 0
