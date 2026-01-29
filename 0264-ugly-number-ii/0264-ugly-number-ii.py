130d26612076ac58a4fc82d45299d12d45f57fbd
Yulo L. <loyu.jobs@gmail.com>
2026-01-28 21:26:52 -0800

    Time: 87 ms (28.89%), Space: 19.8 MB (7.05%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,17 +1,14 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
 
-        #此時他就已經符合heap條件了 在 heapq 裡，「heap」不是一個獨立型別，它就是 一個滿足 heap property 的 list
-        # 已經是合法 min-heap
-        ugly_candidates = [1] 
+        ugly_candidates = [1]
         visited = set([1])
 
-        val = None
         for i in range(n):
-            val = heapq.heappop(ugly_candidates) #所以直接pop沒問題
+            val = heapq.heappop(ugly_candidates)
             for factor in [2,3,5]:
                 if val * factor not in visited:
                     visited.add(val * factor)
                     heapq.heappush(ugly_candidates, val * factor)
+
         return val
-     
