db926463ff86e57f61efa1dbc12c50ee059f8dbe
Yulo L. <loyu.jobs@gmail.com>
2026-01-29 16:16:05 -0800

    Time: 50 ms (29.46%), Space: 19.8 MB (38.98%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -23,6 +23,14 @@ class Solution:
                     oldtonew[nei] = Node(nei.val)
                     queue.append(nei)
             
+                #在複製圖裡，把 curr' -> nei' 這條邊接起來:
+                    #oldtonew[curr]：拿到「curr 這個原本節點」對應的「複製節點 curr'」
+                    #oldtonew[nei]：拿到「nei 這個原本鄰居節點」對應的「複製節點 nei'」
+                    #append(...)：在 curr' 的 neighbors 裡加入 nei'
+                #為什麼不能寫 curr.neighbors.append(nei)
+                    #curr 是 原圖的節點
+                    #nei 是 原圖的鄰居
+                    #curr.neighbors.append(nei) 會把邊加在 原圖 上
                 oldtonew[curr].neighbors.append(oldtonew[nei])
 
         return oldtonew[node] 
