742b86fcc7238105380bd84db6369b6b12aeb08b
Yulo L. <loyu.jobs@gmail.com>
2026-01-29 18:46:20 -0800

    Time: 94 ms (20.57%), Space: 19.7 MB (7.48%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -3,11 +3,12 @@ class Solution:
         ugly_candidates = [1]
         visited = set([1])
 
+        #ㄐfor loop它不控制 candidate 數量，它控制的是 pop 次數（也就是輸出第幾個）
         for i in range(n):
             val = heapq.heappop(ugly_candidates)
             for factor in [2,3,5]:
                 if val * factor not in visited:
-                    visited.add(val * factor)
                     heapq.heappush(ugly_candidates, val * factor)
+                    visited.add(val * factor)
 
         return val
