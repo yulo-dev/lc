e42239cbc511eda7feb1769cd03abebec89344d1
Yulo L. <loyu.jobs@gmail.com>
2026-01-29 18:11:29 -0800

    Time: 35 ms (96.65%), Space: 27.8 MB (61.14%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -3,8 +3,8 @@ class Solution:
         if n == 1:
             return [0]
 
-        graph = [[] for _ in range(n)]
-        in_degree = [0] * n
+        graph = [[] for _ in range(n)] #time: O(n); space: O(n+m)
+        in_degree = [0] * n #time: O(n); space: O(n)
 
         for u, v in edges:
             graph[u].append(v)
@@ -12,7 +12,7 @@ class Solution:
             in_degree[u] += 1
             in_degree[v] += 1
 
-        queue = deque()
+        queue = deque() 
         for i in range(n):
             if in_degree[i] == 1:
                 queue.append(i)
@@ -30,3 +30,8 @@ class Solution:
                         queue.append(nei)
 
         return list(queue) 
+
+#For a tree, we process each node once and each edge a constant number of times, so time is O(n). 
+#We store the graph, degrees, and a queue, so space is O(n).
+#因為這題是 樹, 樹的邊通常在算node的時候就cover進去了, 所以不用分開寫成 n + m
+#但像是lc133 clone graph 因為他是一般圖 不是樹 所以要算 node + edge
