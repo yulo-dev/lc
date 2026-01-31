e7df7e69b87b5c84c1ffffff4d3486d01b581a15
Yulo L. <loyu.jobs@gmail.com>
2026-01-30 23:56:28 -0800

    Time: 0 ms (100%), Space: 19.3 MB (26.49%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        #keyword: remove element, in-place: fast slow pointers
+
+        write = 0
+
+        for read in range(len(nums)):
+            if nums[read] != val:
+                nums[write] = nums[read]
+                write += 1
+
+        return write
