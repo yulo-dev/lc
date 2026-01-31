acd9da627d58d2a4540d700c66fbbdb53fcbf17a
Yulo L. <loyu.jobs@gmail.com>
2026-01-31 15:59:14 -0800

    Time: 3 ms (93.51%), Space: 20.4 MB (11.06%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -2,20 +2,19 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         
         destination = len(nums) - 1
+        farthest = 0 # within current layer, the farthest we can reach for next jump
+        current_end = 0  # with 'steps' jumps, we can reach up to current_end
         steps = 0
-        current_end = 0 #你「用目前這次跳數 steps」已經確定能到的最右邊界（本層邊界）
-        farthest = 0 #「你在掃描本層所有可達位置後，推算出「如果再跳一步」能到的最右邊界（下一層候選邊界）
 
-        for i in range(destination):  # 注意：不用走到最後一格
-            
+        for i in range(destination):
             farthest = max(farthest, i + nums[i])
 
-            #當你掃到 i == current_end，代表：目前這一跳可達的所有位置都評估完了，必須多跳一次才能往更遠去。
-            #沒有 current_end，你就不知道「什麼時候該 +1 跳」
-            if i == current_end:      
+            # When i reaches current_end, we have finished scanning all positions reachable with steps jumps. 
+            # To go further, we must take one more jump, so steps++, and the new reachable boundary becomes farthest.
+            if i == current_end:
                 steps += 1
                 current_end = farthest
-                if current_end >=destination:
+                if current_end >= destination:
                     break
 
         return steps
