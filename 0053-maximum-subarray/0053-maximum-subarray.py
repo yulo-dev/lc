820a9d0c18343b99b3334ec788e067eece276d72
Yulo L. <loyu.jobs@gmail.com>
2026-02-01 12:01:53 -0800

    Time: 51 ms (50.4%), Space: 31.6 MB (42.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #Kadane's Algorithm
-
+        
         curr = nums[0]
         best = nums[0]
 
         for i in range(1, len(nums)):
-            curr = max(nums[i], curr + nums[i]) # restart or extend
-            best = max(best, curr)
+            curr = max(nums[i], curr + nums[i]) #cur =「以 i 結尾」的最大子陣列和
+            best = max(best, curr) #best = 到目前為止（掃到 i 為止）看過的最大子陣列和
 
         return best
