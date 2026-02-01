31a1d4e128799460bef2d97faac5d409141e772f
Yulo L. <loyu.jobs@gmail.com>
2026-02-01 15:47:27 -0800

    Time: 42 ms (22.66%), Space: 23.3 MB (7.71%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,7 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
-        count = defaultdict(int)
+        # Prefix sum + hashmap
+        # Track:
+        #   prefix: running sum up to current index
+        #   freq[p]: how many times prefix sum p has occurred so far
+        # Key idea:
+        #   subarray sum = k  <=>  prefix - old_prefix = k  <=>  old_prefix = prefix - k
+        # So at each step:
+        #   ans += freq[prefix - k]
+        # Then update:
+        #   freq[prefix] += 1
+        # Return:
+        #   ans  (total number of subarrays whose sum equals k)
+
+        count = defaultdict(int) 
         count[0] = 1
 
         prefix = 0
@@ -9,7 +21,8 @@ class Solution:
 
         for x in nums:
             prefix += x
-            ans += count[prefix - k]   # how many previous prefix sums make sum k
-            count[prefix] += 1
+            ans += count[prefix-k] # 先用歷史去算答案
+            count[prefix] += 1 # 再把現在加入歷史 #count是歷史 prefix sums 的頻率表
+           
 
         return ans
