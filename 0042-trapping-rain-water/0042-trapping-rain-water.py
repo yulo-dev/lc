89f7848ddbc0444a3f4e00fc7ac908edbb7f3525
Yulo L. <loyu.jobs@gmail.com>
2026-01-31 23:04:21 -0800

    Time: 15 ms (41.14%), Space: 21.1 MB (22.59%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -3,15 +3,15 @@ class Solution:
         
         left_max = [0] * len(height)
         left_max[0] = height[0]
-        
+
         for i in range(1, len(height)):
-            left_max[i] = max(height[i],  left_max[i - 1])
+             left_max[i] = max(left_max[i-1], height[i])
 
         right_max = [0] * len(height)
         right_max[-1] = height[-1]
-        
+
         for i in range(len(height)-2, -1, -1):
-            right_max[i] = max(height[i],  right_max[i + 1])
+             right_max[i] = max(right_max[i+1], height[i])
 
         tot = 0
         for i in range(len(height)):
