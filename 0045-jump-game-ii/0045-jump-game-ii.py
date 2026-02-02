e92858a90344b91c834ea9c9b2d7d51c24efca9c
Yulo L. <loyu.jobs@gmail.com>
2026-02-01 17:23:02 -0800

    Time: 0 ms (100%), Space: 20.2 MB (31.92%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
         destination = len(nums) - 1
         farthest = 0 # within current layer, the farthest we can reach for next jump
         current_end = 0  # with 'steps' jumps, we can reach up to current_end
@@ -9,12 +8,10 @@ class Solution:
         for i in range(destination):
             farthest = max(farthest, i + nums[i])
 
-            # When i reaches current_end, we have finished scanning all positions reachable with steps jumps. 
-            # To go further, we must take one more jump, so steps++, and the new reachable boundary becomes farthest.
             if i == current_end:
                 steps += 1
                 current_end = farthest
-                if current_end >= destination:
+                if farthest >= destination:
                     break
 
         return steps
