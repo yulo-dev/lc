d492661e64f641c5f3eeb82d70d33f73061f5a82
Yulo L. <loyu.jobs@gmail.com>
2026-02-01 22:04:29 -0800

    Time: 95 ms (14.91%), Space: 22.3 MB (26.68%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,11 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         #keyword: sorting, remove dup, in place: fast-slow pointers
-
+        
         write = 0
-
         for read in range(len(nums)):
-            if write < 2 or write >= 2 and nums[read] != nums[write - 2]:
+            if write < 2 or (write >= 2 and nums[read] != nums[write-2]):
                 nums[write] = nums[read]
                 write += 1
 
