1d945c9d8d9cdcb323bb83a309a69faba38957bb
Yulo L. <loyu.jobs@gmail.com>
2026-02-01 22:47:16 -0800

    Time: 3 ms (80.6%), Space: 19.4 MB (29.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,20 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        #create a map
-        #iterate through s
-
-        mapping = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        sum = 0
-
-        for i, ch in enumerate(s):
-            if i > 0 and (ch == "V" or ch == "X") and s[i-1] == "I":
-                sum -= 2
-            if i > 0 and (ch == "L" or ch == "C") and s[i-1] == "X":
-                sum -= 20
-            if i > 0 and (ch == "D" or ch == "M") and s[i-1] == "C":
-                sum -= 200
-            
-            sum += mapping[ch]
         
-        return sum
-            
+        mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        
+        ans = 0
+        n = len(s)
+
+        for i in range(n):
+            cur = mapping[s[i]]
+            if i + 1 < n and cur < mapping[s[i+1]]:
+                ans -= cur
+            else:
+                ans += cur
+
+        return ans
