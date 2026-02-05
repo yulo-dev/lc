0b9b93394dbd0646cc0bbd37b934183331f30a87
Yulo L. <loyu.jobs@gmail.com>
2026-02-05 12:02:54 -0800

    Time: 254 ms (73.96%), Space: 19.3 MB (57.39%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,7 +4,7 @@ class Solution:
             while 0 <= left and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            return s[left+1:right]
+            return s[left+1:right] #停下來的那一刻，left/right 指向的是“不合法”的那格, 所以「真正最後一次合法的回文」是在它的內側一格。
 
         res = ""
         for i in range(len(s)):
@@ -15,4 +15,5 @@ class Solution:
             even = expand(i,i+1)
             if len(even) > len(res):
                 res = even
+
         return res
