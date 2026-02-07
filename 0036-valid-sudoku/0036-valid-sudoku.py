c234f0efaf599827e497c7b6e7c17a87206eb0cf
Yulo L. <loyu.jobs@gmail.com>
2026-02-06 17:14:12 -0800

    Time: 3 ms (79.13%), Space: 19.5 MB (21.68%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -11,6 +11,14 @@ class Solution:
                 if vals == ".":
                     continue
 
+
+                #固定的映射公式
+                #box row 0: [0] [1] [2]
+                #box row 1: [3] [4] [5]
+                #box row 2: [6] [7] [8]
+
+                #Each 3 rows form a box-row and each 3 cols form a box-col. 
+                #So (i//3, j//3) locates the box in a 3x3 grid, and we flatten it by boxRow*3 + boxCol.
                 b = (i // 3) * 3 + j // 3
 
                 if vals in rows[i] or vals in cols[j] or vals in boxes[b]:
@@ -19,4 +27,5 @@ class Solution:
                 rows[i].add(vals)
                 cols[j].add(vals)
                 boxes[b].add(vals)
+                
         return True
