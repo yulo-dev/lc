4c319629d42e324cb45f47331af0506fc81a7bf5
Yulo L. <loyu.jobs@gmail.com>
2026-02-06 18:06:11 -0800

    Time: 0 ms (100%), Space: 19.4 MB (42.9%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,9 +1,9 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        n = len(board)  # 9
+        n = len(board)
         rows = [set() for _ in range(n)]
         cols = [set() for _ in range(n)]
-        boxes = [set() for _ in range(n)]  # 仍然是 9 個 box（因為 9x9）
+        boxes = [set() for _ in range(n)]
 
         for i in range(n):
             for j in range(n):
@@ -12,15 +12,7 @@ class Solution:
                 if vals == ".":
                     continue
 
-
-                #固定的映射公式
-                #box row 0: [0] [1] [2]
-                #box row 1: [3] [4] [5]
-                #box row 2: [6] [7] [8]
-
-                #Each 3 rows form a box-row and each 3 cols form a box-col. 
-                #So (i//3, j//3) locates the box in a 3x3 grid, and we flatten it by boxRow*3 + boxCol.
-                b = (i // 3) * 3 + j // 3
+                b = (i // 3) * 3 + (j // 3)
 
                 if vals in rows[i] or vals in cols[j] or vals in boxes[b]:
                     return False
