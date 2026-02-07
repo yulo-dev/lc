e36977c0a5146ecb12b66025b1f3d960770de411
Yulo L. <loyu.jobs@gmail.com>
2026-02-07 00:21:35 -0800

    Time: 35 ms (93.16%), Space: 20.2 MB (52.03%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,39 +1,35 @@
 class Solution:
     def calculate(self, s: str) -> int:
         
-        stack = [] # 存外層狀態：先存 res 再存 sign（或反過來也行，但要一致）
-        res = 0   # 目前這一層（目前括號層）已經算好的結果
-        sign = 1 # 下一個數字要用 + 還是 - 加到 res（+1 或 -1）
-        num = 0 # 正在讀的數字（可能是多位數）
+        stack = []
+        sign = 1
+        res = 0
+        num = 0
 
         for ch in s:
-            #1) 遇到數字：累積成多位數
             if ch.isdigit():
-                num = num * 10 + int(ch) #int(ch)也可以用 ord(ch) - ord("0")取代
+                num = num * 10 + (ord(ch) - ord("0"))
 
-            #2) 遇到 + 或 -：把剛剛的 num 結算進 res
-            elif ch in '+-':
-                res += sign * num 
-                num = 0 
-                sign = 1 if ch == '+' else -1
-
-            #3) 遇到 (：把外層狀態存起來，進新層
-            elif ch == '(':
-                stack.append(res) # Save current result and sign to stack
+            elif ch in "+-":
+                res += num * sign
+                num = 0
+                sign = 1 if ch == "+" else -1
+            
+            elif ch == "(":
+                stack.append(res)
                 stack.append(sign)
-                res = 0 # Reset state for inner expression
+                res = 0
+                num = 0 # optional
                 sign = 1
 
-            #4) 遇到 )：把這層算完，然後跟外層合併
-            elif ch == ')':
-                res += sign * num 
-                num = 0
-                prev_sign = stack.pop() 
-                prev_res = stack.pop() 
+            elif ch == ")":
+                res += num * sign
+                prev_sign = stack.pop()
+                prev_res = stack.pop()
                 res = prev_res + prev_sign * res
+                num = 0
+                sign = 1 # optional
 
-            # else: space -> ignore
+        res += num * sign
 
-        #字串結尾不會自動結算，所以最後要再加一次
-        res += sign * num
         return res
