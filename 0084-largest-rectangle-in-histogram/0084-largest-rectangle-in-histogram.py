11ece64d3377ac59be75d559659f4ad2322133f5
Yulo L. <loyu.jobs@gmail.com>
2026-02-08 15:11:31 -0800

    Time: 198 ms (17.69%), Space: 31 MB (63.07%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,20 +1,37 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        heights.append(0) #讓最後一定會把剩下的柱子全部 pop 計算完
-        indices_stack = [] #存 index
-        area = 0
+        n = len(heights)
 
-        for index, height in enumerate(heights ):
-            while indices_stack and heights[indices_stack[-1]] >= height: #如果列表尾部高度大於當前高度
-                popped_index = indices_stack.pop()
-                left_index = indices_stack[-1] if indices_stack else -1		
-                width = index - left_index - 1	 #如果列表是空的，則寬度為index，否則是index-indices_stack[-1]-1
-                area = max(area, width * heights[popped_index])
-                
-            indices_stack.append(index)		
-            
-        return area
+        #注意
+        #它們要表示「找不到更小的邊界」時的 哨兵值
+        #而且這個哨兵值會直接影響寬度公式 width = rightLess[i] - leftLess[i] - 1
+        #leftLess[i] 表示：i 左邊第一個 更小 的 index, 如果左邊根本沒有更小的（i 是目前最小/並列最小
+        #那左邊界就應該視為「在陣列外面」：i=0
+        left_less = [-1] * n
 
-# [1,1] -> 2
-# [0] -> 0
-# [4,2] -> 4  
+        #rightLess[i] 表示：i 右邊第一個 更小 的 index。
+        #如果右邊沒有更小的，就應該視為「在最右邊外面」：→ 等於能延伸到最右端
+        right_less = [n] * n
+
+        stack = []
+        for i in range(n):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            left_less[i] = stack[-1] if stack else -1
+            stack.append(i)
+
+        stack = []
+        for i in range(n-1, -1, -1):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            right_less[i] = stack[-1] if stack else n
+            stack.append(i)
+
+        ans = 0
+        for i in range(n):
+            #left_less[i] 和 right_less[i] 指的是「第一個更小柱子的 index」
+            #那兩根更小的柱子本身 不能包含在矩形寬度裡，所以中間能用的柱子數量要把兩邊各排除掉, 結果就會多出這個 -1
+            width = right_less[i] - left_less[i] - 1
+            ans = max(ans, width * heights[i])
+
+        return ans
