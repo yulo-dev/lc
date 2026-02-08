30f889d485c2dfb2a453525fa036fba579ecab23
Yulo L. <loyu.jobs@gmail.com>
2026-02-07 22:34:03 -0800

    Time: 21 ms (82.44%), Space: 20.1 MB (9.43%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -11,45 +11,38 @@ class Solution:
         need = Counter(words)
         res = []
 
+        # 最困惑的是第一個跟第二個for loop 
+        # 第一個for loop 只會從 0 ~ 單字長度 當作這個window的起始點, 這個window指的是words裡面單字一次排列的組合 不是指單一個單字
+        # 第二個for loop 才會透過每 Ｌ 個跳一次, 去切割他總共跑完整串s, 就是逐個取出長度 L 的 token，掃完整個 Ｓ
+        # left 代表「目前 sliding window 的起始 index」（永遠對齊 offset）
+        # right是單一個token的起點
         for offset in range(L):
             left = offset
-            seen = defaultdict(int) #這個 window 裡每個 word 出現幾次
-            count = 0  # how many words currently in window (validly tracked)
+            seen = defaultdict(int) 
+            count = 0  
 
             for right in range(offset, n - L + 1, L):
                 w = s[right:right+L]
 
-                # 這個 token 根本不在 words 裡，所以任何包含它的 window 都不可能是答案。
-                # 因此我直接「重置」window，從 right+L 重新開始。
-                # 1) 確保window 裡的每個 token 一定是合法單字
                 if w not in need:
-                    seen.clear()
-                    count = 0
+                    seen.clear() #清空 即便剛剛前面有找到幾個吻合的單字 但不能用 因為已經混雜其他不對的單字了 直接清空重新搜集
+                    count = 0 
                     left = right + L
                     continue
 
                 seen[w] += 1
                 count += 1
 
-                # shrink if too many of w
-                # 確保 2) window 裡每個單字的次數都不會超過 need
                 while seen[w] > need[w]:
-                    left_w = s[left:left+L] #找左邊那個 token
-                    seen[left_w] -= 1 #把它從 window 移除
+                    need_remove = s[left:left+L] 
+                    seen[need_remove] -= 1
                     count -= 1
                     left += L
 
-
-                # if window has k words, record
-                # 因為前面做了所有嚴格檢查，最後只剩「長度是否剛好」這個門檻
-                # 因為在進到 if count == k 之前，我們已經確保兩件事：
-                    #1) window 裡的每個 token 一定是合法單字
-                    #2) window 裡每個單字的次數都不會超過 need
                 if count == k:
                     res.append(left)
-                    # move left by one word to continue searching
-                    left_w = s[left:left+L]
-                    seen[left_w] -= 1
+                    need_remove = s[left:left+L]
+                    seen[need_remove] -= 1
                     count -= 1
                     left += L
 
