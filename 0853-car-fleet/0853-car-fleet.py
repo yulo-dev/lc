cba99384de1e78a01ce35a75bb5a388f284b9755
Yulo L. <loyu.jobs@gmail.com>
2026-02-08 00:04:41 -0800

    Time: 148 ms (84.26%), Space: 41.8 MB (30.54%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,13 +1,20 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted(zip(position, speed), reverse=True)  # position big -> small
-        stack = []  # store fleet arrival times
+
+        # 要排序的原因：
+        # 只要你往終點方向看，後車只會追前車；而車隊形成後就等同一台車（不能超車）
+        # 先看離終點最近的車：它前面沒車，一定自己成一隊（暫時）
+        # 再往後看下一台：它只需要問「我追得到前面那個隊嗎？」: 追得到 → 合併; 追不到 → 新車隊
+
+        cars = sorted(zip(position, speed), reverse = True)
+        stack = []
 
         for p, s in cars:
             t = (target - p) / s
-            # if current car catches up to the fleet ahead, merge (do not push new fleet)
-            if stack and t <= stack[-1]:
+
+            if stack and stack[-1] >= t:
                 continue
+            
             stack.append(t)
 
         return len(stack)
