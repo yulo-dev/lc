63cf4dca17afdb49d54ec1068e5a74a65ef0a325
Yulo L. <loyu.jobs@gmail.com>
2026-02-08 18:16:46 -0800

    Time: 0 ms (100%), Space: 19.3 MB (41.07%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -7,6 +7,14 @@ class Solution:
 
          # 1) transpose: swap (i, j) with (j, i) for j > i
          # 先transpose, 欄變列, 列變欄
+         # 他的做法是沿著對角線做「鏡射」: 對角線是 (0,0), (1,1), (2,2)
+         ####### col0   col1   col2
+         #row0   (0,0)  (0,1)  (0,2)
+         #row1   (1,0)  (1,1)  (1,2)
+         #row2   (2,0)  (2,1)  (2,2)
+         # 所以就是(0,1) <-> (1,0), (0,2) <-> (2,0), (1,2) <-> (2,1)
+
+        # 只做上半矩陣
         for i in range(n):
             for j in range(i+1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
