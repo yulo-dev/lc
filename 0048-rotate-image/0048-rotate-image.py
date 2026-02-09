c12ddb3b21a2c1010f9f80667209319e8c56f237
Yulo L. <loyu.jobs@gmail.com>
2026-02-08 18:06:11 -0800

    Time: 0 ms (100%), Space: 19.5 MB (18.93%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,11 +4,14 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        # Step 1 : Transpose： row <-> col
+
+         # 1) transpose: swap (i, j) with (j, i) for j > i
+         # 先transpose, 欄變列, 列變欄
         for i in range(n):
-            for j in range(i + 1, n):
+            for j in range(i+1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
-        # Step 2 : Reverse each row
+
+        # 2) reverse each row
+        # 再每一列反轉
         for row in matrix:
             row.reverse()
