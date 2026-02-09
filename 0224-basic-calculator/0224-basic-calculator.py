ad5608d92f1e90397ea4237c56ef19a7d7c3e0e8
Yulo L. <loyu.jobs@gmail.com>
2026-02-09 00:14:21 -0800

    Time: 35 ms (92.63%), Space: 20.2 MB (61.81%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -3,33 +3,30 @@ class Solution:
         
         stack = []
         sign = 1
-        res = 0
         num = 0
+        res = 0
 
         for ch in s:
             if ch.isdigit():
                 num = num * 10 + (ord(ch) - ord("0"))
-
             elif ch in "+-":
                 res += num * sign
                 num = 0
                 sign = 1 if ch == "+" else -1
-            
             elif ch == "(":
                 stack.append(res)
                 stack.append(sign)
                 res = 0
-                num = 0 # optional
+                num = 0
                 sign = 1
-
             elif ch == ")":
                 res += num * sign
                 prev_sign = stack.pop()
                 prev_res = stack.pop()
-                res = prev_res + prev_sign * res
+                res = prev_res + res * prev_sign
                 num = 0
-                sign = 1 # optional
-
+                sign = 1
+        
         res += num * sign
-
         return res
+                
