6916b6698eb9540db6f8fc9b69d47cc458095052
Yulo L. <loyu.jobs@gmail.com>
2026-02-09 18:24:37 -0800

    Time: 8 ms (92.25%), Space: 22.1 MB (53.8%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        #同一個 key 底下要收集「很多個字串」，所以 value 必須是一個「容器」來裝多個元素——最自然就是 list
+        #不能用defaultdict(str) 因為str是不可變的 無法用append
         mapping = defaultdict(list)
 
-        for s in strs:
-            keys = ''.join(sorted(s)) 
-            mapping[keys].append(s)
+        for ch in strs:
+            key = "".join(sorted(ch))
+            mapping[key].append(ch)
 
         return list(mapping.values())
