7498fe775725f4b23b6ce6c27cca644728ebc7ed
Yulo L. <loyu.jobs@gmail.com>
2026-02-09 17:46:08 -0800

    Time: 0 ms (100%), Space: 19.4 MB (42.89%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -9,29 +9,13 @@ class Solution:
         if len(pattern) != len(words):
             return False
 
-        mapping = defaultdict(int)
-        cnt = 0
-        pattern_1 = []
+        def encode(items):
+            mapping = {}
+            res = []
+            for ch in items:
+                if ch not in mapping:
+                    mapping[ch] = len(mapping)
+                res.append(mapping[ch])
+            return res
 
-        for ch in pattern:
-            if ch in mapping:
-                pattern_1.append(mapping[ch])
-            else:
-                cnt += 1
-                mapping[ch] = cnt 
-                pattern_1.append(mapping[ch])
-
-
-        mapping_2 = defaultdict(int)
-        cnt_2 = 0
-        pattern_2 = []
-
-        for ch in words:
-            if ch in mapping_2:
-                pattern_2.append(mapping_2[ch])
-            else:
-                cnt_2 += 1
-                mapping_2[ch] = cnt_2
-                pattern_2.append(mapping_2[ch])
-
-        return pattern_1 == pattern_2
+        return encode(pattern) == encode(words)
