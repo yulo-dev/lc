d145492229b5a83bc53c595a6adb6cbad2bf4496
Yulo L. <loyu.jobs@gmail.com>
2026-02-11 16:30:38 -0800

    Time: 3 ms (34.14%), Space: 20.5 MB (33.78%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -9,31 +9,31 @@ class Solution:
         #類似lc92 頭插法的寫法
        
         dummy = ListNode(0, head)
-        groupPrev = dummy
+        prev = dummy
 
         while True:
             # 1) 跟lc92有點不同, 因為他要先走K步 確認這組夠長才可以反轉, 所以這邊會走到right boundary
             # 找到這組的第 k 個（kth）
-            kth = groupPrev
+            kth = prev
             for _ in range(k):
                 kth = kth.next
                 if not kth:
-                    return dummy.next  # 不足 k 個就停
+                    return dummy.next  # 不足 k 個就停, 回傳結果, 這邊也是最後跳出while loop的地方
 
-            # 2) groupPrev.next 是這組的頭（curr）
-            # groupPrev：指在「這一組前一個節點」
-            # 所以 這一組的第一個節點 一定是 groupPrev.next
-            curr = groupPrev.next
+            # 2) prev.next 是這組的頭（curr）
+            # prev：指在「這一組前一個節點」
+            # 所以 這一組的第一個節點 一定是 prev.next
+            curr = prev.next
 
-            # 3) 做 k-1 次頭插：把 curr.next 搬到 groupPrev 後面
+            # 3) 做 k-1 次頭插：把 curr.next 搬到 prev 後面
             for _ in range(k - 1):
                 nxt = curr.next
                 curr.next = nxt.next
-                nxt.next = groupPrev.next
-                groupPrev.next = nxt
+                nxt.next = prev.next
+                prev.next = nxt
 
-            # 4) curr 反轉後變成尾巴；把 groupPrev 移到尾巴，準備下一組
-            # curr 一開始是這組的頭（groupPrev.next）
+            # 4) curr 反轉後變成尾巴；把 prev 移到尾巴，準備下一組
+            # curr 一開始是這組的頭（prev.next）
             #你做了 k-1 次頭插，把後面的節點一個個搬到前面
             #結果：這組反轉後，原本的頭 curr 會變成這組的尾巴
-            groupPrev = curr
+            prev = curr
