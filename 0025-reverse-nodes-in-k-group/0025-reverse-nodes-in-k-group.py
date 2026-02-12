1c470fff512f9c2dc395f88f5c825b81d10b73b7
Yulo L. <loyu.jobs@gmail.com>
2026-02-11 17:18:03 -0800

    Time: 5 ms (6.45%), Space: 20.2 MB (65.56%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,6 +5,17 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+
+        #每一輪（每一組 k 個）你都要先鎖定這組的 三個位置：
+            # A = prev：這組「前一個節點」
+            # B = prev.next（也就是 old_head）：這組「第一個節點」（反轉開始點，反轉後會變尾巴）
+            # C = kth：這組「第 k 個節點」（反轉後會變頭）
+            # D = kth.next（也等於 next_start）：這組「後一個節點」（下一組的開始）
+
+            #畫成一條線就是：
+            #... -> A(prev) -> B(prev.next, 組頭) -> ... -> C(kth,組尾) -> D(kth.next) -> ...
+            #要反轉的: 反轉 B 到 C 這段（剛好 k 個）。
         
         dummy = ListNode(0, head)
         prev = dummy
@@ -21,17 +32,17 @@ class Solution:
             new_head, next_start = self.reverse_linked_list(prev.next, k)
 
             # 接回去
-            old_head = prev.next            # 反轉前的頭 -> 反轉後的尾巴
-            old_head.next = next_start      # 尾巴接回下一段
-            prev.next = new_head            # groupPrev 接到新頭
-            prev = old_head                 # prev 移到尾巴，準備下一組
-            
+            old_head = prev.next         # 反轉前的頭 -> 反轉後的尾巴
+            old_head.next = next_start   # B.next = D   (新尾巴接回後面)
+            prev.next = new_head         # A.next = new_head(=C)  (前面接新頭)
+            prev = old_head              # 移到下一組：A 變成這輪的尾巴
+
 
     def reverse_linked_list(self, head, k):
-        previous, current, nxt = None, head, None
+        prev, curr, nxt = None, head, None
         for _ in range(k):
-            nxt = current.next # temporarily store the nxt node
-            current.next = previous # reverse the current node
-            previous = current # before we move to the next node, point previous to the current node
-            current = nxt  # move to the nxt node 
-        return previous, current
+            nxt = curr.next # temporarily store the nxt node
+            curr.next = prev # reverse the current node
+            prev = curr # before we move to the next node, point previous to the current node
+            curr = nxt  # move to the nxt node 
+        return prev, curr
