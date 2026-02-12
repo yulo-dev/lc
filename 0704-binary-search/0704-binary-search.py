1baf8c3b418d7f23b3570763fd3531265825eb96
Yulo L. <loyu.jobs@gmail.com>
2026-02-11 18:02:49 -0800

    Time: 0 ms (100%), Space: 20.5 MB (49.65%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -6,18 +6,14 @@ class Solution:
         left = 0
         right = len(nums) - 1
 
-        while left + 1 < right:
-            mid = left + (right - left) // 2
+        while left <= right:
+            mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                left = mid
-            else:
-                right = mid
 
-        if nums[left] == target:
-            return left
-        if nums[right] == target:
-            return right
-        
-        return - 1
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return -1
