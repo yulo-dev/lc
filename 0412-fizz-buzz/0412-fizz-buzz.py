e3617e5a6cf54ae02db3be07dc48008cce1a6775
Yulo L. <loyu.jobs@gmail.com>
2026-02-13 12:36:39 -0800

    Time: 4 ms (7.57%), Space: 19.7 MB (38.1%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        # initialize result 
+        res = []
+
+        # 1 indexed
+        for i in range(1, n+1):
+            temp = ""
+            if i % 3 == 0:
+                temp += "Fizz" 
+            if i % 5 == 0:
+                temp += "Buzz"
+            if temp == "":
+                temp = str(i)
+
+            res.append(temp)
+
+        return res 
+                
