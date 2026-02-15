6376a89a17856a52e00e7317dd9348e3d6845134
Yulo L. <loyu.jobs@gmail.com>
2026-02-14 17:11:31 -0800

    Time: 0 ms (100%), Space: 19.6 MB (18.69%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,21 +1,20 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        #關鍵在他需要跟nums[right]比較 才能判斷出旋轉
+        #因為「沒旋轉」和「有旋轉」的情況下，跟左邊比的結果是一樣的，但跟右邊比，這兩種情況的結果是相反的，這才能產生判斷訊號
+        #他是用exact value的模板去做微調
+
         left, right = 0, len(nums) - 1
 
-        # 當只有一個元素
-        if len(nums) == 1:
-            return nums[0]
 
-        # 用 left + 1 < right 避免無限循環
-        while left + 1 < right:
+        #注意：這裡用 < 而不是 <=，因為我們要找的是「那一個點」，
+        #當 left == right 時，那個點就是唯一答案。
+        while left < right:
             mid = (left + right) // 2
-
-            # 若中間值大於右端，表示最小值在右邊
+            
             if nums[mid] > nums[right]:
-                left = mid
+                left = mid + 1 # 說明 mid 在左邊的高坡，最小值在右邊，且不可能是 mid
             else:
-                right = mid
-
-        # 離開迴圈後，left 和 right 相鄰
-        # 比較誰比較小
-        return min(nums[left], nums[right])
+                right = mid # 說明 mid 在右邊的低坡，mid 可能是最小值，這裡不能 mid - 1，因為 mid 可能是答案
+                
+        return nums[left]
