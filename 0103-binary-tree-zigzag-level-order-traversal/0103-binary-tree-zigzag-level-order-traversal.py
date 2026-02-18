705d82c00ef64dc7e0f4c3d5ad93c3987293750f
Yulo L. <loyu.jobs@gmail.com>
2026-02-17 19:11:20 -0800

    Time: 0 ms (100%), Space: 19.6 MB (38.72%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -21,17 +21,18 @@ class Solution:
             for _ in range(len(queue)):
                 node = queue.popleft()
                 level.append(node.val)
-
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-
+            
             if REVERSE:
                 level.reverse()
-
+            
             res.append(level)
-        
+            
             REVERSE = not REVERSE
 
         return res
+
+            
