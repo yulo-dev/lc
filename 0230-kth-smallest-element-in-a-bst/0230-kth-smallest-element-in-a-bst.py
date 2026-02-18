712cd4be0dc6598233cc7f4f34ec0625dc3bb11b
Yulo L. <loyu.jobs@gmail.com>
2026-02-17 23:22:26 -0800

    Time: 1 ms (39.5%), Space: 22 MB (72.34%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -4,24 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.cnt = 0
 
-        def inorder(node):
-            if not node:
-                return 
+        # 把 k 存進 self，這樣所有層級的遞迴都能看到同一個 k
+        self.k = k
+        self.res = None
+        
+        self.inorder(root)
+        return self.res
+        
+    def inorder(self, node):
 
-            left = inorder(node.left)
-            if left != None:
-                return left
-            
-            self.cnt += 1
+        # 1. 如果已經找到答案，或者走到空節點，就不用再跑了
+        if not node or self.res is not None:
+            return
 
-            if self.cnt == k:
-                return node.val
+        # 2. 往左走 (最左邊最小)
+        self.inorder(node.left)
 
-            return inorder(node.right)
-
-        return inorder(root)
-            
+        # 3. 中間處理 (點名)
+        self.k -= 1 # 看到一個人，k 就減 1
+        if self.k == 0:
+            self.res = node.val
+            return # 找到了！
+        
+        # 4. 往右走
+        self.inorder(node.right)
