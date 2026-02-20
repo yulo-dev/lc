905c1ce835478c34596b3cecf4eeba9ea98c9efe
Yulo L. <loyu.jobs@gmail.com>
2026-02-20 15:36:49 -0800

    Time: 171 ms (67.64%), Space: 42 MB (51.9%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,38 +1,22 @@
 class MedianFinder:
 
     def __init__(self):
-        # max_heap 存左半部 (存負數來模擬 max heap)
-        self.small = [] 
-        # min_heap 存右半部
-        self.large = []
+        self.small = [] # max-heap via negatives (lower half)
+        self.large = [] # min-heap (upper half)
 
     def addNum(self, num: int) -> None:
-        # 1. 永遠先丟進左邊 (max_heap)
         heapq.heappush(self.small, -num)
-        
-        # 2. 把左邊最強的踢給右邊 (min_heap)
         heapq.heappush(self.large, -heapq.heappop(self.small))
-        
-        # 3. 如果右邊太重了，踢回給左邊
+
         if len(self.large) > len(self.small):
             heapq.heappush(self.small, -heapq.heappop(self.large))
-
-
+      
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
-            # 如果是奇數，左邊那個頂端就是中位數
             return float(-self.small[0])
-        else:
-            # 如果是偶數，取兩邊堆頂的平均
-            return (-self.small[0] + self.large[0]) / 2.0
-
-
-
-#適用heap的原因：
-#1.資料是 streaming 進來的：每次 addNum 後都可能要立刻查 median
-#2.median 其實只關心中間附近：你不需要整個排序好的序列，只需要「中間左邊最大」和「中間右邊最小」
-
         
+        else:
+            return (-self.small[0] + self.large[0]) / 2.0
 
 
 # Your MedianFinder object will be instantiated and called as such:
