21c3d02f3a64730b0ac962f64887301f9100d7a9
Yulo L. <loyu.jobs@gmail.com>
2026-02-19 23:06:11 -0800

    Time: 7 ms (95.99%), Space: 25.5 MB (44.41%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -1,14 +1,21 @@
 class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k 
-        self.h = []
-        for x in nums:
-            self.add(x)
+        # 1. 先把所有數字變成 heap (O(N))
+        self.h = nums
+        heapq.heapify(self.h)
+        
+        # 2. 既然我們要的是「第 K 大」，那我們只需要維持 Heap 的大小為 K
+        # 多的就踢掉 (大的踢掉剩下的就是前 K 大，因為是 Min-Heap)
+        # 這步可做可不做
+        while len(self.h) > k:
+            heapq.heappop(self.h)
       
     def add(self, val: int) -> int:
-        heapq.heappush(self.h, val)
+        heapq.heappush(self.h, val) # O(log K)
+
         if len(self.h) > self.k:
-            heapq.heappop(self.h)
+            heapq.heappop(self.h) # O(log K)
 
         return self.h[0]
      
