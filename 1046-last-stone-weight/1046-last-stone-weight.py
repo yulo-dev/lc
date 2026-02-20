d7d56fbaf338faf1abf33f1cbc5aa21ba2400a1e
Yulo L. <loyu.jobs@gmail.com>
2026-02-19 22:55:15 -0800

    Time: 0 ms (100%), Space: 19.7 MB (7.72%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -11,4 +11,10 @@ class Solution:
                 heapq.heappush(h, -diff)
 
         return -h[0] if len(h) == 1 else 0
-        
+
+
+
+#這題因為要抓max heap, 所以要先把list的數字轉成負的, 再做heapify
+#接著需要至少有兩顆石頭才能比：
+    #兩個最重的石頭 如果有大小之分, 就相減 把這個差值丟回heap
+    #如果兩個重量相同, 就不push回去, 就代表兩個都消失了
