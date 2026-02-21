41e25154c0ce67fa43793db752d5aa8390060958
Yulo L. <loyu.jobs@gmail.com>
2026-02-21 14:13:47 -0800

    Time: 175 ms (21.94%), Space: 20.9 MB (52.18%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -2,23 +2,23 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         if n == 0:
             return len(tasks)
-
+        
         cnt = Counter(tasks)
-        maxFreq = max(cnt.values())                      # 最常見任務的次數
-        numMax = sum(1 for v in cnt.values() if v == maxFreq)  # 有幾種任務都達到 maxFreq
+        h = [-freq for freq in cnt.values()]
+        heapq.heapify(h)
 
-        # 把最高頻任務當骨架：
-        # 共有 (maxFreq - 1) 個「間隔」，每個間隔長度是 (n + 1)
-        # 最後一格要放 numMax 個最高頻任務（因為可能有多個並列最高頻）
-        skeleton = (maxFreq - 1) * (n + 1) + numMax
+        queue = deque()
+        time = 0 
 
-        # 若其他任務很多，能把 idle 填滿，最短時間至少是總任務數
-        # skeleton 是冷卻造成的下限
-        # len(tasks) 是工作量造成的下限
-        return max(len(tasks), skeleton)
+        while h or queue:
+            time += 1
+            if h:
+                freq = heapq.heappop(h) + 1
 
+                if freq != 0:
+                    queue.append((freq, time+n))
+            
+            while queue and queue[0][1] <= time:
+                heapq.heappush(h, queue.popleft()[0])
 
-
-#maxFreq-1：A 之間有幾段要隔開
-#n+1：每段長度（A + 冷卻 n 格）
-#+numMax：最後一排尾巴有幾個並列第一的任務要放
+        return time
