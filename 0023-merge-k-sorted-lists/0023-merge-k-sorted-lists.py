8403ed4704f2f26bd6729f02bbb9598aac85920e
Yulo L. <loyu.jobs@gmail.com>
2026-02-21 09:54:02 -0800

    Time: 3 ms (99.19%), Space: 23.1 MB (35.06%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,43 +5,37 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return None
+        h = []
 
-        while len(lists) > 1:
-            new_list = []     
-            
-            #for loop這邊先負責把「能成對的」通通抓去合併
-                             #為了正常抓到i+1, 這邊記得-1
-            for i in range(0, len(lists) - 1, 2):
-                merged = self.merge_two(lists[i], lists[i+1])
-                new_list.append(merged)
-
-            #這個if 負責把那個「因為減 1 而被迴圈遺忘的落單者」抓回來放進 new_list
-            if len(lists) % 2 == 1:
-                new_list.append(lists[-1])
-            
-            #狀態更新 這樣最外層的while loop才能正確的停下
-            lists = new_list
-
-        return lists[0]
-
-    # 這裡就是最基本的 LC 21 合併兩條有序鏈結串列的邏輯
-    def merge_two(self, a, b):
-
+        for node in lists:
+            if node:
+                #在 Python 的 heapq 裡存入 (node.val, node) 時，如果兩個 node.val 一樣，
+                #Python 會試圖去比較 node 物件的大小，但 ListNode 是不能比較的，這會導致報錯。
+                #解決方法：在 Tuple 中加入一個唯一的編號 i，變成 (node.val, i, node)。
+                #這樣如果數值一樣，它會去比 i，就不會報錯
+                heapq.heappush(h, (node.val, id(node), node))
+        
         dummy = ListNode(0)
         tail = dummy
 
-        while a and b:
-            if a.val < b.val:
-                tail.next = a
-                a = a.next
-            else:
-                tail.next = b
-                b = b.next
+        while h:
+            _, _, node = heapq.heappop(h)
+            tail.next = node
             tail = tail.next
-        
-        tail.next = a if a else b
-        
+
+            if node.next:
+                heapq.heappush(h, (node.next.val, id(node.next), node.next))
+
         return dummy.next
-            
+
+
+#想像你有 k 條已經排好序的隊伍（Linked Lists），每條隊伍的人都由矮到高排列。你要把他們合併成一條大隊伍。
+#直覺作法：每次都看這 k 條隊伍的最前面那個人，選出最矮的那個。
+#為什麼要用 Heap？：如果你有 100 條隊伍，每次都要用 for 迴圈看 100 個頭部節點來找最小值，太慢了O(k)。
+#如果我們把這 k 個頭部節點丟進 Min-Heap，找最小值的時間就縮短成 O(logk)。
+
+#algo:
+    #初始化：把每一條非空的 Linked List 的 第一個節點 (head) 通通丟進 Min-Heap
+    #從 Heap 中 heappop 出最小的節點。這就是我們合併後的下一個節點
+    #如果這個被彈出的節點還有「下一個節點 (node.next)」，立刻把它的下一個節點再丟進 Heap
+    #重複：直到 Heap 空了，合併也就完成了
