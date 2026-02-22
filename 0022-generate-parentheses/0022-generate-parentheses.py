b6cfee72fadec92dfd91b332d50fc1b44d16c454
Yulo L. <loyu.jobs@gmail.com>
2026-02-22 10:57:37 -0800

    Time: 2 ms (35.81%), Space: 19.6 MB (8.29%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,15 +2,27 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         
         res = []
-        queue = deque([("", 0, 0)])
-
-        while queue:
-            substring, open_cnt, close_cnt = queue.popleft()
-            if len(substring) == (n*2):
-                res.append(substring)
-                continue
-            if open_cnt < n:
-                queue.append((substring+"(", open_cnt+1, close_cnt))
-            if close_cnt < open_cnt:
-                queue.append((substring+")", open_cnt, close_cnt+1))
+        path = []
+        
+        # left: 已放了幾個 (
+        # right: 已放了幾個 )
+        def backtrack(left, right):
+            # 1. 終止條件：括號放滿了 (長度達到 2n)
+            if len(path) == 2 * n:
+                res.append("".join(path))
+                return
+            
+            # 2. 選擇一：嘗試放左括號
+            if left < n:
+                path.append("(")
+                backtrack(left + 1, right)
+                path.pop() # 回溯
+                
+            # 3. 選擇二：嘗試放右括號
+            if right < left:
+                path.append(")")
+                backtrack(left, right + 1)
+                path.pop() # 回溯
+        
+        backtrack(0, 0)
         return res
