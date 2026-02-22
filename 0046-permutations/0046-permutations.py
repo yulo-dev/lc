4bcfc53ef8e99ca61a31c0e3c552c410f25b2b19
Yulo L. <loyu.jobs@gmail.com>
2026-02-21 22:44:45 -0800

    Time: 0 ms (100%), Space: 19.6 MB (27.09%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,30 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        visited = [False] * len(nums)
-        self.dfs(nums, visited, [], res)
+        path = []
+        # 1. 點名表：記錄哪些數字已經在 path 裡面了
+        used = [False] * len(nums)
+
+        def backtracking():
+            # 2. 終止條件：當 path 長度跟 nums 一樣，代表排完了！
+            if len(path) == len(nums):
+                res.append(path.copy()) # 拍照存證
+                return
+
+            # 3. 每一層都從頭遍歷所有數字
+            for i in range(len(nums)):
+                # 如果這個數字已經被用過了，跳過它
+                if used[i]:
+                    continue
+
+                # --- 做選擇 (Choose) ---
+                used[i] = True
+                path.append(nums[i])
+                # --- 探索 (Explore) ---
+                backtracking()
+                # --- 撤銷選擇 (Unchoose) ---
+                path.pop()
+                used[i] = False
+
+        backtracking()
         return res
-
-    def dfs(self, nums, visited, subset, res):
-        if len(subset) == len(nums):
-            res.append(subset[:])
-            return
-
-        for i in range(len(nums)):
-            if visited[i] != True:
-                subset.append(nums[i])
-                visited[i] = True
-                self.dfs(nums, visited, subset, res)
-                subset.pop()
-                visited[i] = False
