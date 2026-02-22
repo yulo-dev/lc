fbd029105941af4abae8cfc1886e0fd3cc92293a
Yulo L. <loyu.jobs@gmail.com>
2026-02-22 10:47:35 -0800

    Time: 0 ms (100%), Space: 19.5 MB (33.4%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -7,18 +7,24 @@ class Solution:
         res = []
         path = []
 
-        def backtracking(index, path):
+        def backtracking(index, path): # index 告訴我：現在看第幾個數字
             if len(path) == len(digits):
                 res.append("".join(path))
                 return
 
             current_num = digits[index]
-            letter = mapping[current_num]
+            letter = mapping[current_num] # 拿到這個數字的所有字母（例如 'abc'）
 
-            for c in letter:
+            for c in letter: # 「橫向」嘗試：選 a, 還是 b, 還是 c?
                 path.append(c)
+
+                # 「縱向」深入：不管我選了 a 還是 b，
+                # 下一步都一定是去處理「下一個數字按鍵」(index + 1)
                 backtracking(index+1, path)
                 path.pop()
 
         backtracking(0, [])
         return res
+
+#index (縱向/深度)：代表「現在輪到第幾個數字按鍵了？」（例如：先按 2，再按 3）。
+#for loop (橫向/寬度)：代表「這個按鍵對應的字母，我要選哪一個？」（例如：按 2 時，選 'a'、'b' 還是 'c'？）
