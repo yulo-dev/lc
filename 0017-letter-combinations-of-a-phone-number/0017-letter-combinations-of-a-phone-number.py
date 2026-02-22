193c661d7f153a24e4719ce365ac68b05703967f
Yulo L. <loyu.jobs@gmail.com>
2026-02-22 00:10:16 -0800

    Time: 0 ms (100%), Space: 19.3 MB (62.45%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,27 +1,23 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits: 
+        if not digits:
             return []
+
+        mapping = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
         res = []
         path = []
-        mapping = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
 
         def backtracking(index, path):
-            # 1. 終止條件：當路徑長度等於數字長度，代表湊齊了
             if len(path) == len(digits):
-                res.append("".join(path)) # 字串不用 .copy()，因為 join 會產生新字串
+                res.append("".join(path))
                 return
 
-            # 2. 找出當前數字對應的所有字母
-            current_digit = digits[index]
-            letters = mapping[current_digit]
+            current_num = digits[index]
+            letter = mapping[current_num]
 
-            for char in letters:
-                # 做選擇
-                path.append(char)
-                # 探索：往「下一個數字」移動
-                backtracking(index + 1, path)
-                # 撤銷選擇
+            for c in letter:
+                path.append(c)
+                backtracking(index+1, path)
                 path.pop()
 
         backtracking(0, [])
