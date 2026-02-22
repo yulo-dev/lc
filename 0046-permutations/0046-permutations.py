80d57d9dc782028af36cb3a9c5acebe5e4a50be8
Yulo L. <loyu.jobs@gmail.com>
2026-02-21 22:55:10 -0800

    Time: 0 ms (100%), Space: 19.7 MB (7.6%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,27 +2,23 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
         path = []
-        # 1. 點名表：記錄哪些數字已經在 path 裡面了
         used = [False] * len(nums)
 
         def backtracking():
-            # 2. 終止條件：當 path 長度跟 nums 一樣，代表排完了！
             if len(path) == len(nums):
-                res.append(path.copy()) # 拍照存證
+                res.append(path.copy())
                 return
 
-            # 3. 每一層都從頭遍歷所有數字
             for i in range(len(nums)):
-                # 如果這個數字已經被用過了，跳過它
                 if used[i]:
                     continue
-
-                # --- 做選擇 (Choose) ---
+                
+                #選
                 used[i] = True
                 path.append(nums[i])
-                # --- 探索 (Explore) ---
+                #expand
                 backtracking()
-                # --- 撤銷選擇 (Unchoose) ---
+                #撤回
                 path.pop()
                 used[i] = False
 
