e3a665cb432a6e67beefd21acf66df59a2e41878
Yulo L. <loyu.jobs@gmail.com>
2026-02-22 21:57:41 -0800

    Time: 210 ms (13.07%), Space: 23.1 MB (16.01%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -13,7 +13,7 @@ class Solution:
         for x in range(m):
             for y in range(n):
                 if mat[x][y] == 0:
-                    queue.append((x,y,0))
+                    queue.append((x,y))
                     visited.add((x,y))
                     res[x][y] = 0
         
@@ -22,19 +22,25 @@ class Solution:
 
     def bfs(self, mat, queue, visited, res):
         DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
-
+        dist = 0
         while queue:
-            x, y, dist = queue.popleft()
-            for dir_x, dir_y in DIRECTIONS:
-                new_x = dir_x + x
-                new_y = dir_y + y
+            # 【層級控制】：這一層所有的點，距離 0 的步數都是 dist 
+            # 跟994相同
+            for _ in range(len(queue)):
+                x, y = queue.popleft()
+                for dir_x, dir_y in DIRECTIONS:
+                    new_x = dir_x + x
+                    new_y = dir_y + y
 
-                if not self.is_valid(mat, new_x, new_y, visited):
-                    continue
-                
-                res[new_x][new_y] = dist + 1
-                queue.append((new_x, new_y, dist + 1))
-                visited.add((new_x, new_y))
+                    if not self.is_valid(mat, new_x, new_y, visited):
+                        continue
+                    
+                    res[new_x][new_y] = dist + 1
+                    queue.append((new_x, new_y))
+                    visited.add((new_x, new_y))
+
+            # 這一層處理完，距離加 1
+            dist += 1
 
         return res
 
