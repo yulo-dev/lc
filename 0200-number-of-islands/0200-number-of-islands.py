4d42f33f1be1db991b1940acd548b57570422a6b
Yulo L. <loyu.jobs@gmail.com>
2026-02-22 17:19:10 -0800

    Time: 280 ms (13.91%), Space: 26.9 MB (17.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,42 +5,44 @@ class Solution:
         if not grid or not grid[0]:
             return 0
 
-        island = 0
         visited = set()
+        islands = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i,j) not in visited and grid[i][j] == "1":
+                    self.bfs(i, j, grid, visited)
+                    islands += 1
 
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if (x,y) not in visited and grid[x][y] == "1":
-                    self.bfs(grid, x, y, visited)
-                    island += 1
+        return islands
 
-        return island
-
-    def bfs(self, grid, x, y, visited):
-
+    def bfs(self, x, y, grid, visited):
+        DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
         queue = deque([(x,y)])
         visited.add((x,y))
-        DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
 
         while queue:
             x, y = queue.popleft()
             for dir_x, dir_y in DIRECTIONS:
-                new_x = x + dir_x
-                new_y = y + dir_y
+                new_x = dir_x + x
+                new_y = dir_y + y
 
-                if not self.is_valid(grid, new_x, new_y, visited):
+                if not self.is_valid(new_x, new_y, grid, visited):
                     continue
 
                 queue.append((new_x, new_y))
                 visited.add((new_x, new_y))
 
-    def is_valid(self, grid, x, y, visited):
+
+    def is_valid(self, x, y, grid, visited):
         m = len(grid)
         n = len(grid[0])
 
         if not (0 <= x < m and 0 <= y < n):
             return False
+
         if (x,y) in visited:
             return False
 
         return grid[x][y] == "1"
+            
