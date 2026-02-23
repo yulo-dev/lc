45a78ddee69c8621293936738704210bfef94950
Yulo L. <loyu.jobs@gmail.com>
2026-02-22 21:34:23 -0800

    Time: 7 ms (18.66%), Space: 19.5 MB (10.61%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,44 +3,43 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         if not grid or not grid[0]:
-            return 0
+            return -1
 
         fresh = 0
-        queue = deque([])
+        queue = deque()
         visited = set()
-        
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y] == 1:
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
                     fresh += 1
-                elif grid[x][y] == 2:
-                    queue.append((x,y))
-                    visited.add((x,y))
-        
-        fresh, minutes = self.bfs(grid, queue, visited, fresh)
+                elif grid[i][j] == 2:
+                    queue.append((i,j))
+                    visited.add((i,j))
+
+        if fresh == 0:
+            return 0
+        fresh, minutes = self.bfs(grid, queue, fresh, visited)
         return minutes if fresh == 0 else -1
 
-    def bfs(self, grid, queue, visited, fresh):
-        DIRECTION = [(1,0), (0,1), (-1,0), (0,-1)]
+
+    def bfs(self, grid, queue, fresh, visited):
+        DIRECTIONS = [(1,0), (-1,0), (0,1), (0,-1)]
         minutes = 0
 
         while queue and fresh > 0:
-            # 【最小優化核心】：這分鐘「一開始」有多少爛橘子，就只處理那些
-            # 新產生的爛橘子會被加到 queue 尾端，留到「下一分鐘」再處理
             for _ in range(len(queue)):
                 x, y = queue.popleft()
-                for dir_x, dir_y in DIRECTION:
-                    new_x = dir_x + x
-                    new_y = dir_y + y
-
+                for dir_x, dir_y in DIRECTIONS:
+                    new_x = x + dir_x
+                    new_y = y + dir_y
                     if not self.is_valid(grid, new_x, new_y, visited):
                         continue
-
                     fresh -= 1
-                    queue.append((new_x, new_y))
-                    visited.add((new_x, new_y))
+                    queue.append((new_x,new_y))
+                    visited.add((new_x,new_y))
             minutes += 1
-            
+
         return fresh, minutes
 
     def is_valid(self, grid, x, y, visited):
