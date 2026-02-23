20044d43bcb182ac2b57725301f62ffd035c896f
Yulo L. <loyu.jobs@gmail.com>
2026-02-22 18:02:43 -0800

    Time: 242 ms (63.81%), Space: 21.6 MB (82.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,44 +5,38 @@ class Solution:
         if not grid or not grid[0]:
             return 0
 
-        visited = set()
+        m, n = len(grid), len(grid[0])
         islands = 0
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if (i,j) not in visited and grid[i][j] == "1":
-                    self.bfs(i, j, grid, visited)
+        for r in range(m):
+            for c in range(n):
+                # 1. 發現新島嶼的「起點」
+                if grid[r][c] == "1":
                     islands += 1
-
+                    # 2. 發動 DFS 把它淹沒
+                    self.dfs(grid, r, c)
+        
         return islands
 
-    def bfs(self, x, y, grid, visited):
-        DIRECTIONS = [(1,0), (0,1), (-1,0), (0,-1)]
-        queue = deque([(x,y)])
-        visited.add((x,y))
+    def dfs(self, grid, r, c):
+        # --- 邊界檢查與終止條件 ---
+        # 如果超出邊界，或是這格已經是水 ("0")，就撤退
+        if (r < 0 or c < 0 or 
+            r >= len(grid) or c >= len(grid[0]) or 
+            grid[r][c] == "0"):
+            return
+        
+        # --- 做標記 (淹沒陸地) ---
+        # 把這格改成 "0"，確保外層迴圈和未來的遞迴不會重複計算
+        grid[r][c] = "0"
+        
+        # --- 往四周深挖 (四個方向) ---
+        self.dfs(grid, r + 1, c) # 下
+        self.dfs(grid, r - 1, c) # 上
+        self.dfs(grid, r, c + 1) # 右
+        self.dfs(grid, r, c - 1) # 左
 
-        while queue:
-            x, y = queue.popleft()
-            for dir_x, dir_y in DIRECTIONS:
-                new_x = dir_x + x
-                new_y = dir_y + y
-
-                if not self.is_valid(new_x, new_y, grid, visited):
-                    continue
-
-                queue.append((new_x, new_y))
-                visited.add((new_x, new_y))
-
-
-    def is_valid(self, x, y, grid, visited):
-        m = len(grid)
-        n = len(grid[0])
-
-        if not (0 <= x < m and 0 <= y < n):
-            return False
-
-        if (x,y) in visited:
-            return False
-
-        return grid[x][y] == "1"
-            
+        #我們的目標是「把這團相連的陸地標記完」，標記完了就是完了，這格已經完成了它的使命，
+        #不需要再變回 1 -> 不需要回溯
+        #邏輯： 只要這塊地被我「淹掉」了（從 1 變成 0），它就永遠失去了作為「新起點」的資格。
+        #LC 200 是「破壞性」的搜尋：拆掉這座島，數 1 次，就再也不回頭
