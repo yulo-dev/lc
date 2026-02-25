0f38ceb086e1dc0c2e2d74c6ad0b1b922665ba7b
Yulo L. <loyu.jobs@gmail.com>
2026-02-25 00:20:30 -0800

    Time: 315 ms (33.67%), Space: 20.6 MB (67.52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,36 +2,21 @@ from collections import deque
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: list[str]) -> int:
-        #I converted the wordList into a set to optimize lookup performance. 
-        #In Python, checking for existence in a list takes O(N) time because it requires a linear scan. 
-        #However, a set lookup is an O(1) operation on average because it's implemented using a hash table. 
-        #Since our BFS performs this check for every generated mutation, using a set is essential to prevent a Time Limit Exceeded (TLE) error.
         wordset = set(wordList)
-
         if endWord not in wordset:
             return 0
 
-        # The queue and the visited set are the two fundamental components of this BFS algorithm.
-        # First, the queue manages our search frontier. It stores the words that are waiting to be explored.
-        # Since BFS follows a FIFO (First-In-First-Out) structure, the queue ensures we process the words level-by-level. 
-        # This is exactly how we guarantee that the first time we hit the endWord, it’s via the shortest path.
         queue = deque([beginWord])
-
-
-        # the visited set acts as the memory of our search. It keeps track of all the words we have already encountered. 
-        #Its main purpose is to prevent cycles and redundant computations.
         visited = set([beginWord])
-
-        #The queue tells the algorithm where to go next, while the visited set tells it where not to go back.
-
-        dist = 1
+        dist = 1 # 起點算第一步
 
         while queue:
             for _ in range(len(queue)):
                 word = queue.popleft()
                 if word == endWord:
                     return dist
-                for new_word in self.create_word(word):
+
+                for new_word in self.generate_word(word):
                     if new_word not in wordset or new_word in visited:
                         continue
                     if new_word == endWord:
@@ -42,14 +27,11 @@ class Solution:
 
         return 0
 
-    def create_word(self, word): 
-        words = []
+    def generate_word(self, word):
+        new_word = []
         for i in range(len(word)):
             for ch in 'abcdefghijklmnopqrstuvwxyz':
-                left = word[:i]
-                right = word[i+1:]
-                if ch == word[i]:
+                if word[i] == ch:
                     continue
-                words.append(left + ch + right)
-
-        return words
+                new_word.append(word[:i] + ch + word[i+1:])
+        return new_word
