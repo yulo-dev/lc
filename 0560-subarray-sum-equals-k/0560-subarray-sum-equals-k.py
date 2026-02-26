e94a4a9978514c00c35eb3853d3f3b6634b59f72
Yulo L. <loyu.jobs@gmail.com>
2026-02-25 23:27:24 -0800

    Time: 30 ms (78%), Space: 22 MB (32.69%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,28 +1,24 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # Prefix sum + hashmap
-        # Track:
-        #   prefix: running sum up to current index
-        #   freq[p]: how many times prefix sum p has occurred so far
-        # Key idea:
-        #   subarray sum = k  <=>  prefix - old_prefix = k  <=>  old_prefix = prefix - k
-        # So at each step:
-        #   ans += freq[prefix - k]
-        # Then update:
-        #   freq[prefix] += 1
-        # Return:
-        #   ans  (total number of subarrays whose sum equals k)
-
-        count = defaultdict(int) 
-        count[0] = 1
-
-        prefix = 0
-        ans = 0
-
-        for x in nums:
-            prefix += x
-            ans += count[prefix-k] # 先用歷史去算答案
-            count[prefix] += 1 # 再把現在加入歷史 #count是歷史 prefix sums 的頻率表
-           
-
-        return ans
+        
+        # count: 紀錄 prefix_sum 出現的次數
+        # 初始化 {0: 1} 是為了處理「從 index 0 開始就剛好等於 k」的情況
+        # 如果不放這個，當第一個子陣列 [0...i] 總和剛好等於 k 時，current_sum - k 會等於 0，你會找不到它
+        prefix_map = {0: 1}
+        
+        current_sum = 0
+        result = 0
+        
+        for n in nums:
+            current_sum += n  # 累加當前的前綴和
+            
+            # 找找看過去有沒有一個前綴和，剛好能跟現在湊成 k
+            target = current_sum - k
+            if target in prefix_map:
+                result += prefix_map[target] # 到目前這一步之前，出現過哪些 prefix sum 
+                                             # 所以不能先做 line22的+1 再存入res
+            
+            # 把當前的前綴和存入記帳本，供未來使用
+            prefix_map[current_sum] = prefix_map.get(current_sum, 0) + 1
+            
+        return result
