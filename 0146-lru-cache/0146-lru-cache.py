87aaa9f8ad89adce9052ae70ad77721525e92734
Yulo L. <loyu.jobs@gmail.com>
2026-02-26 21:48:28 -0800

    Time: 120 ms (63.35%), Space: 78.9 MB (12.54%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,16 +1,5 @@
-# get要 O(1) 所以用hashmap去存: 存key - > node 這樣get(key) 才會是O(1) 拿到那個node
-# DLL 才可以讓remove & move to front 都 O(1) 
-# SLL 要把中間某個node拿出來 需要知道他的前一個 常常會 O(n)
-
-# 總結
-# dict 解決：key -> node 的 O(1) 存取
-# DLL 解決：維護「最近使用順序」的 O(1) 移動/刪除
-# → 兩個合起來才做得到 get/put 都 O(1)
-
-
-#DLL node 
 class Node:
-    def __init__(self, key = None, val = None):
+    def __init__(self, key, val):
         self.key = key
         self.val = val
         self.prev = None
@@ -19,10 +8,7 @@ class Node:
 class LRUCache:
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.map = {} # key -> node
-
-        #I use dummy head and dummy tail nodes to simplify insertion and deletion in the doubly linked list. 
-        #They make it easier to add or remove real nodes, because I can always assume there is a node before and after the position I’m updating.
+        self.map = {} #key - > Node
         self.head = Node(0,0)
         self.tail = Node(0,0)
         self.head.next = self.tail
@@ -36,8 +22,8 @@ class LRUCache:
 
     def _add_to_front(self, node):
         first = self.head.next
-        node.prev = self.head
         node.next = first
+        node.prev = self.head
         self.head.next = node
         first.prev = node
 
@@ -48,12 +34,12 @@ class LRUCache:
     def _remove_lru(self):
         lru = self.tail.prev
         self._remove(lru)
-        return lru
+        return lru #for later use when the capacity exceeds
 
     def get(self, key):
         if key not in self.map:
             return -1
-
+            
         node = self.map[key]
         self._move_to_front(node)
         return node.val
@@ -68,25 +54,7 @@ class LRUCache:
         node = Node(key, val)
         self.map[key] = node
         self._add_to_front(node)
-
+        
         if len(self.map) > self.cap:
             lru = self._remove_lru()
             del self.map[lru.key]
-
-        
-
-
-        
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
-
-
-
-
-
-
-# design a Node class for cache: double linkedlist so that put & get can be O(1)
-# within LRUCache: create map, prev, next, in map, key = node, val = node.val
