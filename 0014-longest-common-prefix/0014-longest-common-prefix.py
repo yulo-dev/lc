6b8cf00db41e1e8f4cc462c0563f19fceb5d038a
Yulo L. <loyu.jobs@gmail.com>
2026-02-28 23:15:35 -0800

    Time: 0 ms (100%), Space: 19.3 MB (85.14%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,6 +4,7 @@ class Solution:
 
         for i in range(len(strs[0])):
             for s in strs:
+                # 如果指標超出某個字串長度，或字元不匹配
                 if i == len(s) or strs[0][i] != s[i]:
                     return res
         
