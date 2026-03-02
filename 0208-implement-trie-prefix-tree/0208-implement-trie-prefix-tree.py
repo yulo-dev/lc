1bf5b9231d0806968340a5149c92f2109ac2ca26
Yulo L. <loyu.jobs@gmail.com>
2026-03-02 09:16:25 -0800

    Time: 45 ms (39.26%), Space: 34 MB (65.03%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,7 +1,7 @@
 class TreeNode:
     def __init__(self):
-        self.children = {}
-        self.is_end = False
+        self.children = {} #Key 是字母，Value 是下一個 Trie 節點, children 為什麼是 dict: 因為一個 node 下面可能接很多不同字母，
+        self.is_end = False #用來標記「到這格為止，是否構成一個完整的單字」
 
 class Trie:
     def __init__(self):
@@ -10,9 +10,11 @@ class Trie:
     def insert(self, word: str) -> None:
         cur = self.root
         for ch in word:
+            # 如果字母不在孩子裡，就開一個新的節點
             if ch not in cur.children:
                 cur.children[ch] = TreeNode()
             cur = cur.children[ch]
+        # 最後一個字母標記為結尾
         cur.is_end = True
        
     def search(self, word: str) -> bool:
@@ -21,6 +23,7 @@ class Trie:
             if ch not in cur.children:
                 return False
             cur = cur.children[ch]
+        # 必須走到結尾標記才算找到完整單字
         return cur.is_end
       
     def startsWith(self, prefix: str) -> bool:
