18b773149e99f957818fb5cf25d81e0234437a73
Yulo L. <loyu.jobs@gmail.com>
2026-03-02 21:10:58 -0800

    Time: 165 ms (68.28%), Space: 20.7 MB (41.23%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,25 +1,21 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        res = right
 
         def can_finish(speed):
             hours = 0
             for p in piles:
-                hours += (p + speed - 1) // speed # 向上取整
+                hours += (p + speed - 1) // speed
             return hours <= h
-
-        
-        #最小就是每小時只吃一根 最多就是每小時吃max piles根
-        left = 1
-        right = max(piles) #max piles
-        ans = right # 先存一個保險的答案
 
         while left <= right:
             mid = left + (right - left) // 2
-
             if can_finish(mid):
-                ans = mid       # 這個速度行，試試看能不能更慢
-                right = mid - 1 
+                res = mid
+                right = mid - 1
             else:
-                left = mid + 1  # 太慢了，吃不完，加快速度
+                left = mid + 1
 
-        return ans
+        return res
