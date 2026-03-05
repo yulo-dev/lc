9c5cb8a957887e9384a8d129895d64ee6d3a36e9
Yulo L. <loyu.jobs@gmail.com>
2026-03-05 01:23:53 -0800

    Time: 0 ms (100%), Space: 19.5 MB (76.87%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,44 +1,29 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
         
-        n = len(s)
         s_cnt = Counter(s)
-        max_freq = max(s_cnt.values())
-        if max_freq > (n + 1) // 2:
+
+        if max(s_cnt.values()) > (len(s) + 1) // 2:
             return ""
 
-        h = [(-freq, ch) for ch, freq in s_cnt.items()]
-        heapq.heapify(h)
+        max_heap = []
+        for ch, freq in s_cnt.items():
+            heapq.heappush(max_heap, (-freq, ch))
+
 
         res = []
-        while len(h) >= 2:
-            f1, c1 = heapq.heappop(h)
-            f2, c2 = heapq.heappop(h)
+        used_freq = 0
+        used_ch = ""
 
-            res.append(c1)
-            res.append(c2)
-
-            f1 += 1
-            f2 += 1
-
-            if f1 != 0:
-                heapq.heappush(h, (f1,c1))
-            if f2 != 0:
-                heapq.heappush(h, (f2,c2))
-
-
-        #這邊可以這樣寫 是因為前面先做了這個可行性判斷：
-        #if maxFreq > (n + 1) // 2: return ""
-        #這個判斷保證：最多的字母不會多到需要在最後留下 2 個以上還沒放。
-        #因此最後 heap 裡如果還剩一個元素，它的頻率一定只會剩 1（也就是 freq == -1）
-
-        #為什麼最後不可能剩 2 以上？
-        #因為主迴圈每次都拿兩個不同字母放進 res（用掉 2 個字元），會把高頻字母一直往下消耗、並且逼它跟別的字母交錯。
-        #若某字母最後還能剩 2 個，代表它「一直找不到其他字母來隔開它」，那本質上就會違反可行條件 maxFreq <= (n+1)//2。
-        if h:
-            _, ch = heapq.heappop(h)
+        while max_heap:
+            freq, ch = heapq.heappop(max_heap)
             res.append(ch)
+            freq += 1
+
+            if used_freq < 0:
+                heapq.heappush(max_heap, (used_freq, used_ch))
+
+            used_freq = freq
+            used_ch = ch
 
         return "".join(res)
-
-            
