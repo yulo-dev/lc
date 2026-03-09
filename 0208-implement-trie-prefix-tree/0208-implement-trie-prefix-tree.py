1d4a63212bb0a12ffb54d2f29e9bdd3e9b40bf50
Yulo L. <loyu.jobs@gmail.com>
2026-03-08 17:37:46 -0700

    Time: 28 ms (83.92%), Space: 34.2 MB (63.12%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,58 +1,42 @@
-class TreeNode:
+class TrieNode:
     def __init__(self):
-        #For each Trie node, I store a dictionary called children to map a character to the next node, 
-        #and a boolean is_end to mark whether this node is the end of a complete word.
-        self.children = {} #Key 是字母，Value 是下一個 Trie 節點, children 為什麼是 dict: 因為一個 node 下面可能接很多不同字母，
-        self.is_end = False #用來標記「到這格為止，是否構成一個完整的單字」
+        self.children = {}
+        self.is_end = False
 
 class Trie:
     def __init__(self):
-       #In the Trie class, I initialize a root node. The root does not represent a character itself. It is just the starting point for all words.  
-       self.root = TreeNode()
+        self.root = TrieNode()
 
-
-    #For insertion, I start from the root node and process the word one character at a time.
-    #For each character, I check whether the current node already has that child. If not, I create a new node for that character.
-    #Then I move curr to that child node and continue.
-    #After processing all characters, I mark the last node as is_end = True to indicate that a complete word ends here.
-    def insert(self, word: str) -> None:
+    def insert(self, word):
         cur = self.root
-        for ch in word:
-            # 如果字母不在孩子裡，就開一個新的節點
-            if ch not in cur.children:
-                cur.children[ch] = TreeNode()
 
-            # The line curr = curr.children[ch] means I move my pointer down to the next node for that character.
-            # So I am basically walking down the Trie one level at a time.
+        for ch in word:
+            #這邊是在說我到第幾個位子的這個字母有沒有出現過 不是單純這個字母有沒有出現過而已
+            #如果已經做過app 那apple的時候的app就不用重複建TrieNode
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+
+            #這邊就是把指標往下一個去移動
+            #像是i += 1一樣的概念
             cur = cur.children[ch]
-        # 最後一個字母標記為結尾
+
+        #self.is_end：如果你寫在 Trie 類別裡面，那代表整棵樹只有一個開關
         cur.is_end = True
-       
-    
-    def search(self, word: str) -> bool:
+
+    def search(self, word):
         cur = self.root
         for ch in word:
             if ch not in cur.children:
                 return False
             cur = cur.children[ch]
-        # 必須走到結尾標記才算找到完整單字
-        #If I successfully finish traversing the word, I still need to check curr.is_end.
-        #That is because a path existing in the Trie does not necessarily mean the full word exists. It may only be a prefix.
-        return cur.is_end
-      
 
-    #startsWith is similar to search, but here I only care whether the prefix path exists.
-    #So if I can traverse all characters in the prefix successfully, I return True.
-    def startsWith(self, prefix: str) -> bool:
+        return cur.is_end
+
+    def startsWith(self, prefix):
         cur = self.root
         for ch in prefix:
             if ch not in cur.children:
                 return False
             cur = cur.children[ch]
+
         return True
-        
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
