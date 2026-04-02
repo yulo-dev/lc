5c37a5909611a1833e50137f8da94b3e707a457f
Yulo L. <loyu.jobs@gmail.com>
2026-04-02 12:08:05 -0700

    Time: 3 ms (21.9%), Space: 19.4 MB (98.92%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -3,15 +3,13 @@ class Solution:
         res = []
 
         for i in range(1, n+1):
-            temp = []
-            #因為用了兩個獨立的 if，所以 15 會同時加入 "Fizz" 和 "Buzz"，再用 "".join(temp) 合成 "FizzBuzz"
-            if i % 3 == 0:
-                temp.append("Fizz")
-            if i % 5 == 0:
-                temp.append("Buzz")
-            if not temp:
-                res.append(str(i))
+            if i % 3 == 0 and i % 5 == 0:
+                res.append("FizzBuzz")
+            elif i % 3 == 0:
+                res.append("Fizz")
+            elif i % 5 == 0:
+                res.append("Buzz")
             else:
-                res.append("".join(temp))
+                res.append(str(i))
 
         return res
