70aff23dcd6f5e28d8175c9edb9cc9560cd72512
Yulo L. <loyu.jobs@gmail.com>
2026-04-02 12:06:12 -0700

    Time: 3 ms (21.9%), Space: 19.5 MB (92.73%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,19 +1,17 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        # initialize result 
         res = []
 
-        # 1 indexed
         for i in range(1, n+1):
             temp = []
+            #因為用了兩個獨立的 if，所以 15 會同時加入 "Fizz" 和 "Buzz"，再用 "".join(temp) 合成 "FizzBuzz"
             if i % 3 == 0:
-                temp.append("Fizz") 
+                temp.append("Fizz")
             if i % 5 == 0:
-                temp.append("Buzz")  
+                temp.append("Buzz")
             if not temp:
                 res.append(str(i))
             else:
                 res.append("".join(temp))
 
-        return res 
-                
+        return res
