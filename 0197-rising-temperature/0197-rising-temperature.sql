022b0983666e1dd7f08dffe5e1877ac7528a43e1
Yulo L. <loyu.jobs@gmail.com>
2026-04-16 22:13:30 -0700

    Time: 548 ms (15.12%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -4,3 +4,6 @@ SELECT a.id
 FROM Weather AS a JOIN Weather AS b ON DATEDIFF(a.recordDate, b.recordDate) = 1
 where a.temperature > b.temperature;
 
+#DATEDIFF(a.recordDate, b.recordDate) = 1 這代表a是今天 b是昨天
+#所以where的地方也是 a.temperature > b.temperature 代表 今天溫度 > 昨天溫度
+#所以select的地方要選a.id 也就是今天的
