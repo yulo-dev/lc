c514a8ac7a8e6f1ae74419cdc0e5fa10839a0889
Yulo L. <loyu.jobs@gmail.com>
2026-04-16 20:40:55 -0700

    Time: 437 ms (86.57%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
-select name
-from Customer where referee_id != 2 or referee_id is Null;
+SELECT name
+FROM Customer WHERE referee_id != 2 OR referee_id IS NULL;
