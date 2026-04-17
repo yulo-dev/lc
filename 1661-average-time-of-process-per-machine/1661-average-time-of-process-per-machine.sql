9e7004a14b69cb861d261c2f76aff6c3a5278e4d
Yulo L. <loyu.jobs@gmail.com>
2026-04-17 15:48:41 -0700

    Time: 269 ms (17.46%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -8,3 +8,7 @@ group by a.machine_id;
 
 #要注意avg那邊 他的分母就是根據：這個 machine_id 這一組裡，有幾筆被拿來算平均的資料
 #也就是a.timestamp跟b.timestamp都非空
+#因為只要其中一個是 NULL：
+#3.5 - NULL = NULL
+#NULL - 1.2 = NULL
+#結果就還是 NULL，那這筆就不會被 AVG(...) 算進去。
