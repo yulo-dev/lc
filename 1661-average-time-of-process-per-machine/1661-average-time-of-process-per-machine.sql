5bb920df5cdf20a40ecf66b1bb20fcc8f9eca464
Yulo L. <loyu.jobs@gmail.com>
2026-04-16 23:00:23 -0700

    Time: 255 ms (29.46%), Space: 0B (100%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+SELECT a.machine_id, round(avg(b.timestamp - a.timestamp),3) AS processing_time
+FROM Activity as a left join Activity as b
+ON a.machine_id = b.machine_id and a.process_id = b.process_id
+WHERE a.activity_type = "start" and b.activity_type = "end"
+group by a.machine_id;
+
+#要注意avg那邊 他的分母就是根據：這個 machine_id 這一組裡，有幾筆被拿來算平均的資料
+#也就是a.timestamp跟b.timestamp都非空
