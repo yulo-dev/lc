387bd208937211d8880fd30f481b725ad0448d30
Yulo L. <loyu.jobs@gmail.com>
2026-04-18 10:50:48 -0700

    Time: 525 ms (73.58%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -12,3 +12,7 @@ SELECT ROUND(
 
 FROM first_login AS f LEFT JOIN Activity AS a
 ON f.player_id = a.player_id and datediff(a.event_date, f.first_login_date) = 1;
+
+
+# 如果要單獨算分母 要用 SELECT COUNT(DISTINCT player_id) FROM Activity
+# 把所有 player_id 去重後數總數 → 一個數字，才能當分母。
