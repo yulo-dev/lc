563f147a5e4692be761b898e90f09e6cce7edb92
Yulo L. <loyu.jobs@gmail.com>
2026-04-18 00:44:25 -0700

    Time: 622 ms (26.8%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -9,3 +9,13 @@ SELECT
 
 FROM Transactions
 GROUP BY country, EXTRACT(YEAR FROM trans_date), EXTRACT(MONTH FROM trans_date);
+
+
+# %Y: 四位數年份     -> 2022
+# %y: 兩位數年份    -> 22
+# %m: 兩位數月份（01-12）-> 04
+# %M: 月份英文全名  -> April
+# %d: 兩位數日期（01-31）-> 16
+# %D: 日期加序數    -> 16th
+# %H: 24小時制小時 -> 14
+# %h: 12小時制小時 -> 02
