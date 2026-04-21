15efff77b84d531336feffb57bee275aa500c3da
Yulo L. <loyu.jobs@gmail.com>
2026-04-20 21:54:22 -0700

    Time: 563 ms (63.84%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,21 +1,15 @@
 # Write your MySQL query statement below
 
-SELECT 
-    DATE_FORMAT(trans_date, "%Y-%m") AS month,
-    country, 
-    COUNT(*) as trans_count, SUM(CASE WHEN state = "approved" THEN 1 ELSE 0 END) AS approved_count,
+#get the year & month information as a group
+#total amount per month and year per country
+
+SELECT
+    DATE_FORMAT(trans_date, "%Y-%m") AS month, 
+    country,
+    COUNT(*) AS trans_count, 
+    SUM(CASE WHEN state = "approved" THEN 1 ELSE 0 END) AS approved_count,
     SUM(amount) AS trans_total_amount,
     SUM(CASE WHEN state = "approved" THEN amount ELSE 0 END) AS approved_total_amount
 
 FROM Transactions
-GROUP BY country, EXTRACT(YEAR FROM trans_date), EXTRACT(MONTH FROM trans_date);
-
-
-# %Y: 四位數年份     -> 2022
-# %y: 兩位數年份    -> 22
-# %m: 兩位數月份（01-12）-> 04
-# %M: 月份英文全名  -> April
-# %d: 兩位數日期（01-31）-> 16
-# %D: 日期加序數    -> 16th
-# %H: 24小時制小時 -> 14
-# %h: 12小時制小時 -> 02
+GROUP BY DATE_FORMAT(trans_date, "%Y-%m"), country;
