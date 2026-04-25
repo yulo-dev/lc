8477522384e3ddb47a5b33532d22eef845621b96
Yulo L. <loyu.jobs@gmail.com>
2026-04-25 00:54:07 -0700

    Time: 316 ms (48.93%), Space: 0B (100%) - LeetHub

--- 3673-find-zombie-sessions/3673-find-zombie-sessions.sql
+++ 3673-find-zombie-sessions/3673-find-zombie-sessions.sql
@@ -1,38 +1,29 @@
 # Write your MySQL query statement below
 
+WITH session_metrics AS (
+    SELECT
+        user_id,
+        session_id,
+        TIMESTAMPDIFF(
+            MINUTE,
+            MIN(event_timestamp),
+            MAX(event_timestamp)
+        ) AS session_duration_minutes,
+        SUM(CASE WHEN event_type = 'scroll' THEN 1 ELSE 0 END) AS scroll_count,
+        SUM(CASE WHEN event_type = 'click' THEN 1 ELSE 0 END) AS click_count,
+        SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END) AS purchase_count
+    FROM app_events
+    GROUP BY user_id, session_id
+)
 
-# fulfill all the criteria:
-# duration > 30
-# AND
-# number of event_type = "scroll" >= 5
-# AND
-# number of event_type = "click" / number of event_type = "scroll" 
-# AND
-# number of event_type = "purchase" = 0
-
-
-# combine all
 SELECT
-    session_id, 
-    user_id, 
-    TIMESTAMPDIFF(MINUTE, MIN(event_timestamp), MAX(event_timestamp)) AS session_duration_minutes,
-    SUM(event_type = 'scroll') AS scroll_count 
-    #SUM(CASE WHEN event_type = 'scroll' THEN 1 ELSE 0 END) AS scroll_count
-
-FROM app_events
-GROUP BY user_id, session_id
-HAVING
-    TIMESTAMPDIFF(MINUTE, MIN(event_timestamp), MAX(event_timestamp)) > 30
-    AND
-    SUM(event_type = 'scroll') >= 5
-    AND 
-    SUM(event_type = 'click') / NULLIF(SUM(event_type = 'scroll'), 0) < 0.2
-    AND
-    SUM(event_type = 'purchase') = 0
-
-ORDER BY scroll_count DESC, session_id
-;
-
-#能一起做的前提就是 grain 一樣
-#全部都是 GROUP BY user_id, session_id，所以一個 GROUP BY + HAVING 就能全部解決
-#grain 一樣 → 一個 query 搞定
+    session_id,
+    user_id,
+    session_duration_minutes,
+    scroll_count
+FROM session_metrics
+WHERE session_duration_minutes > 30
+  AND scroll_count >= 5
+  AND click_count * 1.0 / scroll_count < 0.20
+  AND purchase_count = 0
+ORDER BY scroll_count DESC, session_id;
