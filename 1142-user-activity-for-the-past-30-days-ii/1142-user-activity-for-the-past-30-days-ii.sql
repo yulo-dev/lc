2976a2aa4fd2201b5e934b9bfadd609ba0ea01af
Yulo L. <loyu.jobs@gmail.com>
2026-04-24 22:25:02 -0700

    Time: 427 ms (62.32%), Space: 0B (100%) - LeetHub

--- 1142-user-activity-for-the-past-30-days-ii/1142-user-activity-for-the-past-30-days-ii.sql
+++ 1142-user-activity-for-the-past-30-days-ii/1142-user-activity-for-the-past-30-days-ii.sql
@@ -14,23 +14,16 @@ WITH within_window AS (
         DISTINCT user_id, session_id
     FROM Activity 
     WHERE DATEDIFF('2019-07-27', activity_date) <= 29
-),
-
-count_numerator AS (
-    SELECT 
-        COUNT(*) AS numerator
-    FROM within_window
-    GROUP BY user_id, session_id
 )
 
 SELECT
     ROUND(
         COALESCE(
-            SUM(numerator) * 1.0 
+            count(*) * 1.0 
             / 
             NULLIF(
-                (SELECT COUNT(DISTINCT user_id) FROM within_window)
+                COUNT(DISTINCT user_id)
                 ,0)
         ,0)
     ,2) AS average_sessions_per_user
-FROM count_numerator; 
+FROM within_window;
