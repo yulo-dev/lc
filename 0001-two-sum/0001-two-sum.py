84683498713fd86b3e09e4ad478e9ddd8f76b231
Yulo L. <loyu.jobs@gmail.com>
2026-05-12 17:21:10 -0700

    Time: 0 ms (100%), Space: 20.3 MB (61.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        arr = [(num, i) for i, num in enumerate(nums)]
-        arr.sort()  # 按 num 排序
+        visit = {} #{nums: index}
+        for i, n in enumerate(nums):
+            comp = target - n
+            if comp in visit:
+                return [visit[comp], i]
 
-        left = 0
-        right = len(arr) - 1
-
-        while left < right:
-            curr_sum = arr[left][0] + arr[right][0]
-
-            if curr_sum == target:
-                return [arr[left][1], arr[right][1]]
-            elif curr_sum < target:
-                left += 1
-            else:
-                right -= 1
+            visit[n] = i
