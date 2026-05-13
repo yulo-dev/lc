134829ea034d097c4389b0e9bb0a28dc19afd25d
Yulo L. <loyu.jobs@gmail.com>
2026-05-12 17:01:16 -0700

    Time: 7 ms (91.01%), Space: 19.5 MB (27.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        s_cnt = defaultdict(int)
-        t_cnt = defaultdict(int)
+        return Counter(s) == Counter(t)
 
-        for ch in s:
-            s_cnt[ch] += 1
-
-        for ch in t:
-            t_cnt[ch] += 1
-
-
-        return s_cnt == t_cnt
+#Python 的 dict（包含 Counter）在做 == 比較時，只看「是不是每個 key 都有，而且對應的 value 都一樣」，完全不管 key 的順序
