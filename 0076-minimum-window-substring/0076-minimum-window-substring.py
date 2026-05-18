10b903ff1385a37c006024f3ecd73d497a694b5e
Yulo L. <loyu.jobs@gmail.com>
2026-05-18 01:34:32 -0700

    Time: 134 ms (26.21%), Space: 19.8 MB (12.48%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -6,21 +6,22 @@ class Solution:
         t_freq = Counter(t)
         min_len = float("inf")
         res = ""
+        comp = 0
 
         for right in range(len(s)):
             window_freq[s[right]] += 1
-            while self.is_valid(window_freq, t_freq):
+
+            if s[right] in t_freq and window_freq[s[right]] == t_freq[s[right]]:
+                comp += 1
+
+            while comp >= len(t_freq):
                 if right - left + 1 < min_len:
                     min_len = right - left + 1
                     res = s[left:right+1]
 
                 window_freq[s[left]] -= 1
+                if s[left] in t_freq and window_freq[s[left]] < t_freq[s[left]]:
+                    comp -= 1
                 left += 1
 
         return res
-
-    def is_valid(self, window_freq, t_freq):
-        for ch, freq in t_freq.items():
-            if ch not in window_freq or freq > window_freq[ch]:
-                return False
-        return True
