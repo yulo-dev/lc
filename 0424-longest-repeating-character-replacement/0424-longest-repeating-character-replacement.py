ef039d99eb5f1d7eda5685f44bac9f1a21ac07e5
Yulo L. <loyu.jobs@gmail.com>
2026-05-17 21:11:25 -0700

    Time: 151 ms (22.47%), Space: 19.7 MB (51.05%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -14,3 +14,17 @@ class Solution:
             res = max(res, right - left + 1)
 
         return res
+
+
+# 擴張
+#freq[s[right]] += 1       # 加進來
+
+# 收縮
+#freq[s[left]] -= 1        # 拿出去
+#left += 1                 # 移動邊界
+
+
+#while (right - left + 1) - max(freq.values()) > k:
+    #We shrink the window when the window length minus the count of the most frequent character is greater than k. 
+    #This means we need more than k replacements to make all characters the same, 
+    #so the window is invalid and we need to shrink it.
