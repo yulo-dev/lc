bc0748f8f96f714ad7218576c9610553417c0565
Yulo L. <loyu.jobs@gmail.com>
2026-05-20 20:11:48 -0700

    Time: 0 ms (100%), Space: 19.4 MB (21.91%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -9,16 +9,14 @@ class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return
-        
-        root.left, root.right = root.right, root.left
 
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
 
         return root
-
-
-#這題 preorder 跟 postorder 都能用，但 inorder 不行。
-#Preorder（先交換再遞迴）： 交換左右 → 遞迴左 → 遞迴右 
-#Postorder（先遞迴再交換）： 遞迴左 → 遞迴右 → 交換左右 
-#Inorder（遞迴左 → 交換 → 遞迴右）： 左邊反轉完後交換到右邊，又被反轉一次 
