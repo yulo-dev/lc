b14bc2ed1e9fc35fe4c875b6b7cda0f3d9632365
Yulo L. <loyu.jobs@gmail.com>
2026-05-21 15:57:43 -0700

    Time: 69 ms (13.15%), Space: 24.4 MB (54.98%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,23 +7,18 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root or root == p or root == q:
-            return root
+        if not root:
+            return None
 
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        if root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)  
+        right = self.lowestCommonAncestor(root.right, p, q)  
 
         if left and right:
             return root
-
-        return left if left else right
-
-
-#My approach is to use DFS recursion.
-#For each node, I recursively search the left and right subtrees.
-#The key idea is that the recursive call returns whether it found p, q, or the LCA candidate in that subtree.
-
-#There are three important cases:
-    #If the current node is None, I return None.
-    #If the current node is p or q, I return the current node.
-    #After searching left and right, if both sides return non-null, that means p and q are found in different subtrees, so the current node is their lowest common ancestor.
+        elif left:
+            return left
+        else:
+            return right
