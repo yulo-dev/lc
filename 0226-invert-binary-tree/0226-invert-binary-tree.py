4987de05e6cb8e6a97d681739befa8a8fae8c35e
Yulo L. <loyu.jobs@gmail.com>
2026-05-20 20:07:19 -0700

    Time: 0 ms (100%), Space: 19.2 MB (89.93%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -16,3 +16,9 @@ class Solution:
         self.invertTree(root.right)
 
         return root
+
+
+#這題 preorder 跟 postorder 都能用，但 inorder 不行。
+#Preorder（先交換再遞迴）： 交換左右 → 遞迴左 → 遞迴右 
+#Postorder（先遞迴再交換）： 遞迴左 → 遞迴右 → 交換左右 
+#Inorder（遞迴左 → 交換 → 遞迴右）： 左邊反轉完後交換到右邊，又被反轉一次 
