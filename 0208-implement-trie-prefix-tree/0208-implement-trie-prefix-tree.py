2da78ebb1e4fc802ec9f90ef21378447c58b753a
Yulo L. <loyu.jobs@gmail.com>
2026-05-22 16:15:02 -0700

    Time: 33 ms (69.6%), Space: 34.1 MB (52.62%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,42 +1,35 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
+        self.children = {} 
         self.is_end = False
-
+       
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
+        self.root = TrieNode() # 整棵 Trie 的起點
 
     def insert(self, word):
-        cur = self.root
-
-        for ch in word:
-            #這邊是在說我到第幾個位子的這個字母有沒有出現過 不是單純這個字母有沒有出現過而已
-            #如果已經做過app 那apple的時候的app就不用重複建TrieNode
-            if ch not in cur.children:
-                cur.children[ch] = TrieNode()
-
-            #這邊就是把指標往下一個去移動
-            #像是i += 1一樣的概念
-            cur = cur.children[ch]
-
-        #self.is_end：如果你寫在 Trie 類別裡面，那代表整棵樹只有一個開關
-        cur.is_end = True
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = TrieNode()
+            curr = curr.children[w]
+        curr.is_end = True
 
     def search(self, word):
-        cur = self.root
-        for ch in word:
-            if ch not in cur.children:
+        curr = self.root
+        for w in word:
+            if w not in curr.children:
                 return False
-            cur = cur.children[ch]
+            curr = curr.children[w]
 
-        return cur.is_end
-
+        return curr.is_end == True
+   
     def startsWith(self, prefix):
-        cur = self.root
-        for ch in prefix:
-            if ch not in cur.children:
+        curr = self.root
+
+        for p in prefix:
+            if p not in curr.children:
                 return False
-            cur = cur.children[ch]
+            curr = curr.children[p]
 
         return True
