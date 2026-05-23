bd989cc834c286b5e9bed28d18a70da2da9fc796
Yulo L. <loyu.jobs@gmail.com>
2026-05-22 21:55:58 -0700

    Time: 3 ms (87.3%), Space: 20.2 MB (93.13%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,12 +1,13 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for _ in range(numCourses)]
-        in_degree = [0] * numCourses
 
+        graph = [[] for _ in range(numCourses)]
+        in_degree = defaultdict(int)
+        
         for course, prereq in prerequisites:
             graph[prereq].append(course)
             in_degree[course] += 1
-
+            
         queue = deque()
         for i in range(numCourses):
             if in_degree[i] == 0:
@@ -24,4 +25,3 @@ class Solution:
 
         return num_complete == numCourses
 
- 
