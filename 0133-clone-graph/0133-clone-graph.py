9fd89dc9b87866b044e7884656dc03252f6d8944
Yulo L. <loyu.jobs@gmail.com>
2026-05-25 21:09:45 -0700

    Time: 53 ms (34.27%), Space: 19.4 MB (99.74%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -6,46 +6,26 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-#要複製
-    #node的value
-    #neighbor關係
-    #整個graph structure
-
-#核心考點
-    #graph不是tree, 他有cycle 或可能 多個node指向同個node
-    #需要用hashmap 其中key=舊的node value=對應的新的clone node 
-    #hashmap用來防止
-        #重複clone
-        #防止cycle
-
-
 
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        
-        # use bfs or dfs to traverse the original graph, and use a hashmap to store a mapping from the original node to its cloned node
-        # use a queue to store the current node
-        # bfs
-        # traverse its neighbor
-            # if the neighbor hasn't been add in the map: add in the map & queue
-        # connect the new cloned node with new cloned neighbor
-        # return the cloned starting node
-
         if not node:
             return None
         
-        oldtonew = {}
-        oldtonew[node] = Node(node.val)
+        old_new = {}
+        #左邊 old_new[node] → 把舊 node 當 key，新 node 當 value 存進 dict
+        #右邊 Node(node.val) → 建一個全新的 Node，val 跟舊的一樣，但 neighbors 是空的 []
+        old_new[node] = Node(node.val)
         queue = deque([node])
-
+        
         while queue:
             curr = queue.popleft()
             for nei in curr.neighbors:
-                if nei not in oldtonew:
-                    oldtonew[nei] = Node(nei.val)
-                    queue.append(nei) # 原本graph的neighbor node
+                if nei not in old_new:
+                    old_new[nei] = Node(nei.val)
+                    queue.append(nei)
                 
-                oldtonew[curr].neighbors.append(oldtonew[nei])
+                old_new[curr].neighbors.append(old_new[nei])
 
-        return oldtonew[node]
+        return old_new[node]
