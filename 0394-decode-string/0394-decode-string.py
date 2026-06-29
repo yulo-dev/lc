956ef1c4c3e9235b17626ea0b0d6b9440662b484
Yulo L. <loyu.jobs@gmail.com>
2026-06-29 07:54:44 -0700

    Time: 0 ms (100%), Space: 19.3 MB (38.69%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,25 +1,29 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         
-        stack_ch = []
-        stach_num = []
+        # numeric
+        # alphabets
+        # [
+        # ]
 
-        curr_ch = ""
+        stack = []
+        curr_alpha = ""
         curr_num = 0
 
         for ch in s:
-            if ch.isdigit():
-                curr_num = curr_num * 10 + int(ch)
-            elif ch == "[":
-                stack_ch.append(curr_ch)
-                stach_num.append(curr_num)
-                curr_ch = ""
-                curr_num = 0
-            elif ch == "]":
-                prev_ch = stack_ch.pop()
-                repeat_time = stach_num.pop()
-                curr_ch = prev_ch + repeat_time * curr_ch
-            else:
-                curr_ch = curr_ch + ch
+            if ch.isalpha():
+                curr_alpha += ch
 
-        return curr_ch
+            elif ch.isnumeric():
+                curr_num = curr_num * 10 + int(ch)
+
+            elif ch == "[":
+                stack.append((curr_num, curr_alpha))
+                curr_alpha = ""
+                curr_num = 0
+                
+            else:
+                time, alpha = stack.pop()
+                curr_alpha = alpha + time * curr_alpha
+
+        return curr_alpha
