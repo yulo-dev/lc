ca7e8d487d432f6d336250c0f0b67e662b351d0f
Yulo L. <loyu.jobs@gmail.com>
2026-07-01 00:00:31 +0800

    Time: 143 ms (28.58%), Space: 19.7 MB (14.81%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,30 +1,22 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         
+        # sliding window
+        # valid: freq of other = k
+
         left = 0
         freq = defaultdict(int)
         res = 0
+
         for right in range(len(s)):
             freq[s[right]] += 1
-            
+
             while (right - left + 1) - max(freq.values()) > k:
                 freq[s[left]] -= 1
                 left += 1
 
             res = max(res, right - left + 1)
-
+            
         return res
 
-
-# 擴張
-#freq[s[right]] += 1       # 加進來
-
-# 收縮
-#freq[s[left]] -= 1        # 拿出去
-#left += 1                 # 移動邊界
-
-
-#while (right - left + 1) - max(freq.values()) > k:
-    #We shrink the window when the window length minus the count of the most frequent character is greater than k. 
-    #This means we need more than k replacements to make all characters the same, 
-    #so the window is invalid and we need to shrink it.
+        
